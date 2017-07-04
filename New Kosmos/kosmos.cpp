616587a7ebf094ae701bb87482765ce5a10a902b
Matthew Albrecht <mattparks5855@gmail.com>
2017-07-04 01:26:56 -0500

    Current progress

--- New Kosmos/kosmos.cpp
+++ New Kosmos/kosmos.cpp
@@ -2,48 +2,12 @@
 //
 
 #include <iostream>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 
 #include "framework.h"
 
-void error_callback(int error, const char* description)
-{
-	fprintf(stderr, "Error: %s\n", description);
-}
-
 int main()
 {
-	if (!glfwInit())
-	{
-		// Initialization failed
-		std::cout << "GLFW failed to initalize!" << std::endl;
-	}
-
-	glfwSetErrorCallback(error_callback);
-
-	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-	if (!window)
-	{
-		// Window or OpenGL context creation failed
-	}
-
-	glfwMakeContextCurrent(window);
-
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-		int width, height;
-		glfwGetFramebufferSize(window, &width, &height);
-		glViewport(0, 0, width, height);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glClearColor(1.0, 0.0, 0.0, 1.0);
-
-		glfwSwapBuffers(window);
-	}
-
-	glfwDestroyWindow(window);
-	glfwTerminate();
-
+	framework framework = new framework((std::string) "New Kosmos", NULL, 60, new extension[0]);
+	framework.run();
     return 0;
 }
