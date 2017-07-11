fd7f9cf0e357bff6dfeba3e36d67ed52279e94b2
Matthew Albrecht <mattparks5855@gmail.com>
2017-07-10 19:18:14 -0500

    Worked on things

---
+++ Flounder Engine/Sources/lights/attenuation.cpp
@@ -0,0 +1,31 @@
+#include "attenuation.h"
+
+namespace flounder {
+	attenuation::attenuation(const float constant, const float linear, const float exponent)
+	{
+		set(constant, linear, exponent);
+	}
+
+	attenuation::attenuation(attenuation *source)
+	{
+		set(source);
+	}
+
+	attenuation::~attenuation()
+	{
+	}
+
+	void attenuation::set(const float constant, const float linear, const float exponent)
+	{
+		m_constant = constant;
+		m_linear = linear;
+		m_exponent = exponent;
+	}
+
+	void attenuation::set(attenuation *source)
+	{
+		m_constant = source->m_constant;
+		m_linear = source->m_linear;
+		m_exponent = source->m_exponent;
+	}
+}
---
+++ Flounder Engine/Sources/lights/light.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "../maths/colour.h"
+#include "../maths/vector3.h"
+#include "attenuation.h"
+
+namespace flounder {
+	/// <summary>
+	/// Represents a point light, contains a colour, position and attenuation.
+	/// </summary>
+	class light
+	{
+	public:
+		colour *m_colour;
+		vector3 *m_position;
+		attenuation *m_attenuation;
+
+		/// <summary>
+		/// Creates a new point light with unlimited range.
+		/// </summary>
+		/// <param name="colour"> The colour of the light. </param>
+		/// <param name="position"> The world position of the light. </param>
+		light(colour *colour, vector3 *position);
+
+		/// <summary>
+		/// Creates a new point light.
+		/// </summary>
+		/// <param name="colour"> The colour of the light. </param>
+		/// <param name="position"> The world position of the light. </param>
+		/// <param name="attenuation"> How much the intensity of the light is lost over a distance. </param>
+		light(colour *colour, vector3 *position, attenuation *attenuation);
+
+		/// <summary>
+		/// Deconstructor for light.
+		/// </summary>
+		~light();
+	};
+}
