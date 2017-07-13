98f4ed502080c5d25e4e4a1357e2cc0457600c58
Matthew Albrecht <mattparks5855@gmail.com>
2017-07-13 14:28:31 -0500

    Added some model loading

--- Flounder Engine/Sources/loaders/loaders.h
+++ Flounder Engine/Sources/loaders/loaders.h
@@ -13,7 +13,7 @@ namespace flounder {
 	class loaders : public imodule
 	{
 	private:
-		std::map<GLuint*, std::vector<int>*> *m_loaded;
+		std::map<GLuint, std::vector<GLuint>*> *m_loaded;
 	public:
 		static loaders *get()
 		{
@@ -27,5 +27,31 @@ namespace flounder {
 		void init();
 
 		void update();
+
+		/// <summary>
+		/// Creates an empty VAO.
+		/// </summary>
+		/// <returns> The ID of the VAO. </returns>
+		GLuint createVAO();
+
+		/// <summary>
+		/// Creates an index buffer and binds it to a VAO.
+		/// </summary>
+		/// <param name="vaoID"> The ID of the VAO to which the index buffer should be bound. </param>
+		/// <param name="indices"> The array of indices to be stored in the index buffer.
+		/// </param>
+		/// <returns> The ID of the index buffer VBO. </returns>
+		GLuint createIndicesVBO(GLuint vaoID, int *indices);
+
+		/// <summary>
+		/// Stores a float array of data into a FBO.
+		/// </summary>
+		/// <param name="vaoID"> The VAO to create a new FBO in. </param>
+		/// <param name="data"> The data to store. </param>
+		/// <param name="attributeNumber"> The attribute to create the FBO under. </param>
+		/// <param name="coordSize"> The size of data being store.
+		/// </param>
+		/// <returns> The new FBO's ID. </returns>
+		GLuint storeDataInVBO(GLuint vaoID, float *data, int attributeNumber, int coordSize);
 	};
 }
