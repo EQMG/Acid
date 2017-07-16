4d107a30a8336dc5e6210c733fe7b7bd39d96c29
Matthew Albrecht <mattparks5855@gmail.com>
2017-07-15 21:46:13 -0500

    More files

---
+++ FlounderCore/Sources/devices/audio.cpp
@@ -0,0 +1,42 @@
+#include "audio.h"
+
+namespace flounder {
+	audio::audio() : 
+		imodule()
+	{
+	}
+
+	audio::~audio()
+	{
+	}
+
+	void audio::init()
+	{
+		// Creates the OpenAL contexts.
+		m_device = alcOpenDevice(NULL);
+
+		m_context = alcCreateContext(m_device, NULL);
+		ALCboolean made = alcMakeContextCurrent(m_context);
+
+		// Checks for errors.
+		ALenum alError = alGetError();
+
+		if (alError != GL_NO_ERROR) 
+		{
+			std::cout << "Failed to load OpenAL!" << std::endl << alError << std::endl;
+		}
+
+		// Creates a new sound model.
+		alDistanceModel(AL_LINEAR_DISTANCE_CLAMPED);
+
+	}
+
+	void audio::update()
+	{
+		if (camera::get()->getCamera() != NULL) 
+		{
+			vector3 *cameraPosition = camera::get()->getCamera()->getPosition();
+			alListener3f(AL_POSITION, cameraPosition->x, cameraPosition->y, cameraPosition->z);
+		}
+	}
+}
