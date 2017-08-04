5b2f8cad210b34b3d122054caa70c4bd19dc199c
Matthew Albrecht <mattparks5855@gmail.com>
2017-08-04 00:36:27 -0500

    Added all perlin, perlin tests
    Former-commit-id: da1ade0f19e6ecfca24e59e182db1e2388e745a2

--- Flounder-Core/src/noise/noiseperlin.h
+++ Flounder-Core/src/noise/noiseperlin.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../helpers/helperarray.h"
+#include "../maths/maths.h"
 
 namespace flounder
 {
@@ -14,15 +15,197 @@ namespace flounder
 	class noiseperlin
 	{
 	private:
+		static const int B;
+		static const int BM;
+		static const int N;
+		static const int SAMPLE_SIZE;
+
+		int *m_pimp;
+		int *m_p;
+		float *m_g1;
+		float **m_g2;
+		float **m_g3;
 
 		int m_seed;
 	public:
-		noiseperlin(const int seed);
+		noiseperlin(const int &seed);
 
 		~noiseperlin();
 
-		inline int getSeed() const { return m_seed; }
+		/// <summary>
+		/// Create noise in a 1D space using the original noise noise algorithm.
+		/// </summary>
+		/// <param name="x"> The X coordinate of the location to sample.
+		/// </param>
+		/// <returns> A noisy value at the given position. </returns>
+		float noise(const float &x);
 
-		inline void setSeed(const int seed) { m_seed = seed; }
+		/// <summary>
+		/// Create noise in a 2D space using the original noise noise algorithm.
+		/// </summary>
+		/// <param name="x"> The X coordinate of the location to sample. </param>
+		/// <param name="y"> The Y coordinate of the location to sample.
+		/// </param>
+		/// <returns> A noisy value at the given position. </returns>
+		float noise(const float &x, const float &y);
+
+		/// <summary>
+		/// Create noise in a 3D space using the original noise noise algorithm.
+		/// </summary>
+		/// <param name="x"> The X coordinate of the location to sample. </param>
+		/// <param name="y"> The Y coordinate of the location to sample. </param>
+		/// <param name="z"> The Z coordinate of the location to sample.
+		/// </param>
+		/// <returns> A noisy value at the given position. </returns>
+		float noise(const float &x, const float &y, const float &z);
+
+		/// <summary>
+		/// Computes noise function for three dimensions at the point (x,y,z).
+		/// </summary>
+		/// <param name="x"> x dimension parameter. </param>
+		/// <param name="y"> y dimension parameter. </param>
+		/// <param name="z"> z dimension parameter.
+		/// </param>
+		/// <returns> the noise value at the point (x, y, z). </returns>
+		float improvedNoise(const float &x, const float &y, const float &z);
+
+		/// <summary>
+		/// Create a 1D tileable noise function for the given width.
+		/// </summary>
+		/// <param name="x"> The X coordinate to generate the noise for. </param>
+		/// <param name="w"> The width of the tiled block.
+		/// </param>
+		/// <returns> The value of the noise at the given coordinate. </returns>
+		float tileableNoise(const float &x, const float &w);
+
+		/// <summary>
+		/// Create a 2D tileable noise function for the given width and height.
+		/// </summary>
+		/// <param name="x"> The X coordinate to generate the noise for. </param>
+		/// <param name="y"> The Y coordinate to generate the noise for. </param>
+		/// <param name="w"> The width of the tiled block. </param>
+		/// <param name="h"> The height of the tiled block.
+		/// </param>
+		/// <returns> The value of the noise at the given coordinate. </returns>
+		float tileableNoise(const float &x, const float &y, const float &w, const float &h);
+
+		/// <summary>
+		/// Create a 3D tileable noise function for the given width, height and depth.
+		/// </summary>
+		/// <param name="x"> The X coordinate to generate the noise for. </param>
+		/// <param name="y"> The Y coordinate to generate the noise for. </param>
+		/// <param name="z"> The Z coordinate to generate the noise for. </param>
+		/// <param name="w"> The width of the tiled block. </param>
+		/// <param name="h"> The height of the tiled block. </param>
+		/// <param name="d"> The depth of the tiled block.
+		/// </param>
+		/// <returns> The value of the noise at the given coordinate. </returns>
+		float tileableNoise(const float &x, const float &y, const float &z, const float &w, const float &h, const float &d);
+
+		/// <summary>
+		/// Create a turbulence function in 1D using the original noise noise function.
+		/// </summary>
+		/// <param name="x"> The X coordinate of the location to sample. </param>
+		/// <param name="freq"> The frequency of the turbulence to create.
+		/// </param>
+		/// <returns> The value at the given coordinates. </returns>
+		float turbulence(const float &x, const float &freq);
+
+		/// <summary>
+		/// Create a turbulence function in 2D using the original noise noise function.
+		/// </summary>
+		/// <param name="x"> The X coordinate of the location to sample. </param>
+		/// <param name="y"> The Y coordinate of the location to sample. </param>
+		/// <param name="freq"> The frequency of the turbulence to create.
+		/// </param>
+		/// <returns> The value at the given coordinates. </returns>
+		float turbulence(const float &x, const float &y, const float &freq);
+
+		/// <summary>
+		/// Create a turbulence function in 3D using the original noise noise function.
+		/// </summary>
+		/// <param name="x"> The X coordinate of the location to sample. </param>
+		/// <param name="y"> The Y coordinate of the location to sample. </param>
+		/// <param name="z"> The Z coordinate of the location to sample. </param>
+		/// <param name="freq"> The frequency of the turbulence to create.
+		/// </param>
+		/// <returns> The value at the given coordinates. </returns>
+		float turbulence(const float &x, const float &y, const float &z, const float &freq);
+
+		/// <summary>
+		/// Create a turbulent noise output based on the core noise function. This uses the noise as a base function and is suitable for creating clouds, marble and explosion effects. For example, a typical marble effect would set the colour to be:
+		/// <pre>
+		/// sin(point + turbulence(point) * point.x);
+		/// </pre>
+		/// </summary>
+		/// <param name="x"> The X coordinate of the location to sample. </param>
+		/// <param name="y"> The Y coordinate of the location to sample. </param>
+		/// <param name="z"> The Z coordinate of the location to sample. </param>
+		/// <param name="loF"> The lower location. </param>
+		/// <param name="hiF"> The upper location.
+		/// </param>
+		/// <returns> The value at the given coordinates. </returns>
+		float improvedTurbulence(const float &x, const float &y, const float &z, const float &loF, const float &hiF);
+
+		/// <summary>
+		/// Create a turbulence function that can be tiled across a surface in 2D.
+		/// </summary>
+		/// <param name="x"> The X coordinate of the location to sample. </param>
+		/// <param name="y"> The Y coordinate of the location to sample. </param>
+		/// <param name="w"> The width to tile over. </param>
+		/// <param name="h"> The height to tile over. </param>
+		/// <param name="freq"> The frequency of the turbulence to create.
+		/// </param>
+		/// <returns> The value at the given coordinates. </returns>
+		float tileableTurbulence(const float &x, const float &y, const float &w, const float &h, const float &freq);
+
+		/// <summary>
+		/// Create a turbulence function that can be tiled across a surface in 3D.
+		/// </summary>
+		/// <param name="x"> The X coordinate of the location to sample. </param>
+		/// <param name="y"> The Y coordinate of the location to sample. </param>
+		/// <param name="z"> The Z coordinate of the location to sample. </param>
+		/// <param name="w"> The width to tile over. </param>
+		/// <param name="h"> The height to tile over. </param>
+		/// <param name="d"> The depth to tile over. </param>
+		/// <param name="freq"> The frequency of the turbulence to create.
+		/// </param>
+		/// <returns> The value at the given coordinates. </returns>
+		float tileableTurbulence(const float &x, const float &y, const float &z, const float &w, const float &h, const float &d, const float &freq);
+	private:
+		/// <summary>
+		/// Fade curve calculation which is 6t^5 - 15t^4 + 10t^3. This is the new algorithm, where the old one used to be 3t^2 - 2t^3.
+		/// </summary>
+		/// <param name="t"> The t parameter to calculate the fade for.
+		/// </param>
+		/// <returns> the drop-off amount. </returns>
+		float fade(const float &t);
+
+		/// <summary>
+		/// Calculate the gradient function based on the hash code.
+		/// </summary>
+		float grad(const int &hash, const float &x, const float &y, const float &z);
+
+		/// <summary>
+		/// S-curve function for value distribution for Perlin-1 noise function.
+		/// </summary>
+		float sCurve(const float &t);
+
+		/// <summary>
+		/// Simple lerp function using floats.
+		/// </summary>
+		float lerp(const float &t, const float &a, const float &b);
+
+		/// <summary>
+		/// Simple bias generator using exponents.
+		/// </summary>
+		float bias(const float &a, const float &b);
+
+		/// <summary>
+		/// Gain generator that caps to the range of [0, 1].
+		/// </summary>
+		float gain(const float &a, const float &b);
+	public:
+		inline int getSeed() const { return m_seed; }
 	};
 }
--- Flounder-Core/src/terrains/terrain.cpp
+++ Flounder-Core/src/terrains/terrain.cpp
@@ -5,8 +5,10 @@ namespace flounder
 	const double terrain::SQUARE_SIZE = 2.598f;
 	const int terrain::VERTEX_COUNT = 176;
 
-	terrain::terrain(const vector3 &position, const vector3 &rotation)
+	terrain::terrain(const vector3 &position, const vector3 &rotation, const int &seed)
 	{
+		m_noise = new noiseperlin(seed);
+
 		m_model = NULL;
 
 		m_position = new vector3(position);
@@ -21,6 +23,8 @@ namespace flounder
 
 	terrain::~terrain()
 	{
+		delete m_noise;
+
 		delete m_model;
 
 		delete m_position;
@@ -42,7 +46,6 @@ namespace flounder
 	void terrain::generateMesh()
 	{
 		int count = VERTEX_COUNT * VERTEX_COUNT;
-		noisesimplex noise = noisesimplex(0420);
 		std::vector<GLfloat> *vertices = new std::vector<GLfloat>();
 		std::vector<GLfloat> *normals = new std::vector<GLfloat>();
 		std::vector<GLfloat> *colours = new std::vector<GLfloat>();
@@ -58,23 +61,9 @@ namespace flounder
 		{
 			for (int row = 0; row < VERTEX_COUNT; row++)
 			{
-				vector3 position = calculatePosition(row, col, noise);
-				vector3 normal = calculateNormal(row, col, noise);
-				
-				colour *tint = colour::interpolate(colourDirt, colourGrass, std::min(fabs(normal.m_y), 1.0f), NULL);
-
-				if (position.m_y - 20.0f > 0.0f)
-				{
-					colour::interpolate(*tint, colourSnow, std::min(fabs(position.m_y - 20.0f), 1.0f), tint);
-				}
-				else if (position.m_y - 17.0f > 0.0f)
-				{
-					colour::interpolate(*tint, colourStone, std::min(fabs(position.m_y - 17.0f), 1.0f), tint);
-				}
-				else if (position.m_y < 2.0f)
-				{
-					colour::interpolate(*tint, colourSand, std::min(fabs(position.m_y), 1.0f), tint);
-				}
+				vector3 position = calculatePosition(row, col);
+				vector3 normal = calculateNormal(row, col);
+				colour tint = getBiomeColour(row, col);
 
 				storeVertex(vertices, position);
 
@@ -82,11 +71,9 @@ namespace flounder
 				normals->push_back(normal.m_y);
 				normals->push_back(normal.m_z);
 
-				colours->push_back(tint->m_r);
-				colours->push_back(tint->m_g);
-				colours->push_back(tint->m_b);
-
-				delete tint;
+				colours->push_back(tint.m_r);
+				colours->push_back(tint.m_g);
+				colours->push_back(tint.m_b);
 			}
 		}
 
@@ -112,9 +99,11 @@ namespace flounder
 
 		m_model = model::newModel()->setDirectly(indices, vertices, NULL, normals, colours)->create();
 
-		m_position->m_x -= m_aabb->m_maxExtents->m_x / 2.0f;
+		/*m_position->m_x -= m_aabb->m_maxExtents->m_x / 2.0f;
 		m_position->m_z -= m_aabb->m_maxExtents->m_z / 2.0f;
 		m_aabb->update(*m_position, *m_rotation, 1.0f, m_aabb);
+		std::cout << m_aabb->m_maxExtents->m_x / 2.0f << std::endl;
+		std::cout << m_aabb->m_maxExtents->m_z / 2.0f << std::endl;*/
 
 		delete vertices;
 		delete normals;
@@ -176,24 +165,197 @@ namespace flounder
 		indices->push_back(mixed ? topLeft : topRight);
 	}
 
-	vector3 terrain::calculateNormal(const int &x, const int &z, noisesimplex &noise)
+	vector3 terrain::calculateNormal(const int &x, const int &z)
 	{
-		vector3 positionL = calculatePosition(x - 1, z, noise);
-		vector3 positionR = calculatePosition(x + 1, z, noise);
-		vector3 positionD = calculatePosition(x, z - 1, noise);
-		vector3 positionU = calculatePosition(x, z + 1, noise);
+		vector3 positionL = calculatePosition(x - 1, z);
+		vector3 positionR = calculatePosition(x + 1, z);
+		vector3 positionD = calculatePosition(x, z - 1);
+		vector3 positionU = calculatePosition(x, z + 1);
 
 		vector3 normal = vector3(positionL.m_y - positionR.m_y, 2.0f, positionD.m_y - positionU.m_y);
 		normal.normalize();
 		return normal;
 	}
 
-	vector3 terrain::calculatePosition(const int &x, const int &z, noisesimplex &noise)
+	colour terrain::getBiomeColour(const int &x, const int &z)
 	{
-		float worldX = static_cast<float>(x) * SQUARE_SIZE;
-		float worldZ = static_cast<float>(z) * SQUARE_SIZE;
-		float worldY = 25.0f * noise.noise(worldX / 115.0f, worldZ / 115.0f) + 8.0f;
-		return vector3(worldX, worldY, worldZ);
+		float worldX = (static_cast<float>(x) * SQUARE_SIZE) + m_position->m_x;
+		float worldZ = (static_cast<float>(z) * SQUARE_SIZE) + m_position->m_z;
+
+		float height = calculatePosition(x, z).m_y;
+		float moisture = getFactorMoisture(worldX, height, worldZ);
+
+		if (height <= 0.125f)
+		{
+			// Ocean.
+			return colour(0.0824f, 0.3960f, 0.7530f);
+		}
+		else if (height <= 0.25f)
+		{
+			if (moisture <= 0.16f)
+			{
+				// Subtropical Desert.
+				return colour(0.914f, 0.8275f, 0.7804f);
+			}
+			else if (moisture <= 0.33f)
+			{
+				// Grassland.
+				return colour(0.7686f, 0.8314f, 0.6666f);
+			}
+			else if (moisture <= 0.66f)
+			{
+				// Tropical Seasonal Forest.
+				return colour(0.6627f, 0.8000f, 0.6431f);
+			}
+			else if (moisture <= 1.0f)
+			{
+				// Tropical Rain Forest.
+				return colour(0.6431f, 0.7686f, 0.6588f);
+			}
+		}
+		else if (height <= 0.5f)
+		{
+			if (moisture <= 0.16f)
+			{
+				// Temperate Desert.
+				return colour(0.8941f, 0.9098f, 0.7922f);
+			}
+			else if (moisture <= 0.5f)
+			{
+				// Grassland.
+				return colour(0.7686f, 0.8314f, 0.6666f);
+			}
+			else if (moisture <= 0.83f)
+			{
+				// Temperate Deciduous Forest.
+				return colour(0.7059f, 0.7882f, 0.6627f);
+			}
+			else if (moisture <= 1.0f)
+			{
+				// Temperate Rain Forest.
+				return colour(0.6431f, 0.7686f, 0.6588f);
+			}
+		}
+		else if (height <= 0.75f)
+		{
+			if (moisture <= 0.33f)
+			{
+				// Temperate Desert.
+				return colour(0.8941f, 0.9098f, 0.7922f);
+			}
+			else if (moisture <= 0.66f)
+			{
+				// Shrubland.
+				return colour(0.7686f, 0.8000f, 0.7333f);
+			}
+			else if (moisture <= 1.0f)
+			{
+				// Taiga.
+				return colour(0.8000f, 0.8314f, 0.7333f);
+			}
+		}
+		else if (height <= 1.0f)
+		{
+			if (moisture <= 0.16f)
+			{
+				// Scorched.
+				return colour(0.6000f, 0.6000f, 0.6000f);
+			}
+			else if (moisture <= 0.33f)
+			{
+				// Bare.
+				return colour(0.7333f, 0.7333f, 0.7333f);
+			}
+			else if (moisture <= 0.5f)
+			{
+				// Tundra.
+				return colour(0.8666f, 0.8666f, 0.7333f);
+			}
+			else if (moisture <= 1.0f)
+			{
+				// Snow.
+				return colour(1.0000f, 1.0000f, 1.0000f);
+			}
+		}
+
+		return colour(0.0824f, 0.3960f, 0.7530f);
+	}
+
+	float terrain::getFactorIsland(const float &x, const float &z)
+	{
+		const int worldSize = 1536;
+		const float worldIslandInside = 0.8f;
+		const float worldIslandOutside = 1.0f;
+		const float worldIslandParameter = 0.4f;
+
+		float circular = static_cast<float>(sqrt(pow(x, 2) + pow(z, 2))); // The current radius (circular map).
+		float rectangular = __max(fabs(x), fabs(z)); // The current radius (rectangular map).
+		float reading = ((1.0f - worldIslandParameter) * circular) + (worldIslandParameter * rectangular);
+
+		float radius1 = worldIslandInside * (worldSize / 2.0f); // The inside radius to the blur.
+		float radius2 = worldIslandOutside * (worldSize / 2.0f); // The outside radius to the blur.
+		
+		if (x == 0.0f && z == 0.0f) // The special case where the reading is undefined.
+		{
+			return 1.0f;
+		} 
+		else if (reading > radius2) // If outside the upper bound there is no factor!
+		{
+			return 0.0f;
+		} 
+		else if (reading >= radius1) // Something between upper and lower, uses cos interpolation.
+		{
+			float blend = maths::clamp((reading - radius1) / (radius2 - radius1), 0.0f, 1.0f);
+			return maths::clamp(maths::cosInterpolate(1.0f, 0.0f, blend), 0.0f, 1.0f);
+		} 
+		else // Fully inside of the lower radius, so full factor.
+		{
+			return 1.0f;
+		}
+	}
+
+	float terrain::getFactorMoisture(const float & x, const float & y, const float & z)
+	{
+		const float worldNoiseHeight = 40.0f;
+
+		// Calculate the moisture as a inverse of height with added noise.
+		float moisture = y / worldNoiseHeight;
+		
+		// Set to 100% moisture in the ocean/lakes/rivers.
+		if ((y / worldNoiseHeight) <= 0.0f)
+		{
+			moisture = 1.0f;
+		}
+		else
+		{
+			moisture += m_noise->turbulence(x / 150.0f, z / 150.0f, 16.0f);
+		}
+
+		moisture = maths::clamp(moisture, 0.0f, 1.0f);
+
+		return maths::clamp(moisture, 0.0f, 1.0f);
+	}
+
+	vector3 terrain::calculatePosition(const int &x, const int &z)
+	{
+		float worldX = (static_cast<float>(x) * SQUARE_SIZE) + m_position->m_x;
+		float worldZ = (static_cast<float>(z) * SQUARE_SIZE) + m_position->m_z;
+
+		const float worldNoiseSpread = 400.0f;
+		const float worldNoiseFrequency = 40.0f;
+		const float worldNoiseHeight = 40.0f;
+		const float dayNightCycle = 600.0f;
+		const float dayNightRatio = 0.7f;
+
+		float island = getFactorIsland(worldX, worldZ);
+		float height = island * 1.70f * m_noise->turbulence(
+			worldX / worldNoiseSpread,
+			worldZ / worldNoiseSpread,
+			worldNoiseFrequency
+		);
+		height *= worldNoiseHeight;
+
+		return vector3(worldX, height, worldZ);
 	}
 
 	void terrain::setPosition(const vector3 &position)
