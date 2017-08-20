91989d3cab796e39b31d5c11a62002391c673e41
Matthew Albrecht <mattparks5855@gmail.com>
2017-08-20 01:38:37 -0500

    Added a transform class

---
+++ Flounder-Core/src/test/renderertest.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "../renderer/irenderer.h"
+
+namespace flounder
+{
+	class renderertest :
+		public irenderer
+	{
+	private:
+		shader *m_shader;
+	public:
+		renderertest();
+
+		~renderertest();
+
+		void render(const vector4 &clipPlane, const icamera &camera) override;
+	};
+}
