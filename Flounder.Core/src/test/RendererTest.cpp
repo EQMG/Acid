c2cf2a642a654e23efa851d7f8d029a487363010
Matthew Albrecht <mattparks5855@gmail.com>
2017-10-04 06:47:46 -0500

    Seperated some UBOs and entities for testing
    Former-commit-id: 20b6308254ffeb673a3b6c974819830464add74a

--- Flounder.Core/src/test/RendererTest.cpp
+++ Flounder.Core/src/test/RendererTest.cpp
@@ -2,6 +2,7 @@
 
 #include "../devices/Display.hpp"
 #include "../renderer/Renderer.hpp"
+#include "TestUbos.hpp"
 
 namespace Flounder
 {
@@ -9,16 +10,13 @@ namespace Flounder
 		IRenderer(),
 		m_uniformScene(UniformBuffer(sizeof(UboScene), VK_SHADER_STAGE_VERTEX_BIT)),
 
-		m_uniformObject(UniformBuffer(sizeof(UboObject), VK_SHADER_STAGE_VERTEX_BIT)),
-		m_model(Model("res/treeBirchSmall/model.obj")),
-		m_diffuse(Texture("res/treeBirchSmall/diffuse.png", VK_SHADER_STAGE_FRAGMENT_BIT)),
-		m_swapMap(Texture("res/treeBirchSmall/sway.png", VK_SHADER_STAGE_VERTEX_BIT)),
+		m_testEntity(TestEntity()),
 
 		m_shader(Shader("tests", {
 			ShaderType(VK_SHADER_STAGE_VERTEX_BIT, "res/shaders/tests/test.vert.spv"),
 			ShaderType(VK_SHADER_STAGE_FRAGMENT_BIT, "res/shaders/tests/test.frag.spv")
 		})),
-		m_pipeline(Pipeline("tests", PipelinePolygon, &m_shader, { &m_uniformScene, &m_uniformObject, &m_diffuse, &m_swapMap }))
+		m_pipeline(Pipeline("tests", PipelinePolygon, &m_shader, { &m_uniformScene, &m_testEntity.m_uniformObject, &m_testEntity.m_diffuse, &m_testEntity.m_swapMap }))
 	{
 		auto bindingDescription = Vertex::GetBindingDescription();
 		auto attributeDescriptions = Vertex::GetAttributeDescriptions();
@@ -29,10 +27,7 @@ namespace Flounder
 		vertexInputState.pVertexAttributeDescriptions = attributeDescriptions.data();
 
 		m_uniformScene.Create();
-		m_uniformObject.Create();
-		m_model.Create();
-		m_diffuse.Create();
-		m_swapMap.Create();
+		m_testEntity.Create();
 
 		vkDeviceWaitIdle(Display::Get()->GetLogicalDevice());
 		vkQueueWaitIdle(Display::Get()->GetQueue());
@@ -46,10 +41,7 @@ namespace Flounder
 		m_shader.Cleanup();
 		m_pipeline.Cleanup();
 
-		m_swapMap.Cleanup();
-		m_diffuse.Cleanup();
-		m_model.Cleanup();
-		m_uniformObject.Cleanup();
+		m_testEntity.Cleanup();
 		m_uniformScene.Cleanup();
 	}
 
@@ -60,23 +52,6 @@ namespace Flounder
 		uboScene.view = *camera.GetViewMatrix();
 		m_uniformScene.Update(&uboScene);
 
-		UboObject uboObject = {};
-		uboObject.model = Matrix4();
-		uboObject.swaying = 1.0f;
-		const float systemTime = Engine::Get()->GetTime();
-		const float swayX = 0.24f * (sin(0.25f * systemTime) - sin(1.2f * systemTime) + cos(0.5f * systemTime));
-		const float swayY = 0.24f * (cos(0.25f * systemTime) - cos(1.2f * systemTime) + sin(0.5f * systemTime));
-		uboObject.swayOffset = Vector2(swayX, swayY);
-		Matrix4::TransformationMatrix(Vector3(0.0f, -2.3f, 3.0f), Vector3(), 1.0f, &uboObject.model);
-		m_uniformObject.Update(&uboObject);
-
-		VkBuffer vertexBuffers[] = { m_model.GetVertexBuffer().GetBuffer() };
-		VkDeviceSize offsets[] = { 0 };
-		VkDescriptorSet descriptors[] = { m_pipeline.GetDescriptorSet() };
-		vkCmdBindPipeline(*commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipeline.GetPipeline());
-		vkCmdBindDescriptorSets(*commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipeline.GetPipelineLayout(), 0, 1, descriptors, 0, nullptr);
-		vkCmdBindVertexBuffers(*commandBuffer, 0, 1, vertexBuffers, offsets);
-		vkCmdBindIndexBuffer(*commandBuffer, m_model.GetIndexBuffer().GetBuffer(), 0, m_model.GetIndexBuffer().GetIndexType());
-		vkCmdDrawIndexed(*commandBuffer, m_model.GetIndexBuffer().GetIndexCount(), 1, 0, 0, 0);
+		m_testEntity.CmdRender(*commandBuffer, m_pipeline);
 	}
 }
---
+++ Flounder.Core/src/test/TestEntity.hpp
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "../renderer/buffers/UniformBuffer.hpp"
+#include "../textures/Texture.hpp"
+#include "../models/Model.hpp"
+#include "../renderer/pipelines/Pipeline.hpp"
+
+namespace Flounder
+{
+	class TestEntity
+	{
+	public:
+		UniformBuffer m_uniformObject;
+		Model m_model;
+		Texture m_diffuse;
+		Texture m_swapMap;
+
+		TestEntity();
+
+		~TestEntity();
+
+		void Create();
+
+		void Cleanup();
+
+		void CmdRender(const VkCommandBuffer &commandBuffer, const Pipeline &pipeline);
+	};
+}
