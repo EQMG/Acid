5d1a1533c122e4c2a2dbc30567b42bf902ca36a9
Matthew Albrecht <mattparks5855@gmail.com>
2017-10-08 11:34:25 -0500

    Added terrain.

--- Flounder.Core/src/terrains/Terrain.cpp
+++ Flounder.Core/src/terrains/Terrain.cpp
@@ -1,6 +1,10 @@
 ﻿#include "Terrain.hpp"
 
+#include "../maths/Colour.hpp"
 #include "../worlds/Worlds.hpp"
+#include "../devices/Display.hpp"
+#include "../skyboxes/ShaderSkyboxes.hpp"
+#include "ShaderTerrains.hpp"
 
 namespace Flounder
 {
@@ -9,6 +13,7 @@ namespace Flounder
 	const float Terrain::SIDE_LENGTH = 0.5f * SQUARE_SIZE * static_cast<float>(VERTEX_COUNT - 1);
 
 	Terrain::Terrain(const Vector3 &position, const Vector3 &rotation, const int &seed) :
+		m_uniformObject(new UniformBuffer(sizeof(ShaderTerrains::UboObject))),
 		m_model(nullptr),
 		m_position(new Vector3(position)),
 		m_rotation(new Vector3(rotation)),
@@ -39,41 +44,50 @@ namespace Flounder
 		}
 	}
 
+	void Terrain::CmdRender(const VkCommandBuffer & commandBuffer, const Pipeline & pipeline, const UniformBuffer & uniformScene)
+	{
+		const auto logicalDevice = Display::Get()->GetLogicalDevice();
+		const auto descriptorSet = pipeline.GetDescriptorSet();
+
+		std::vector<VkWriteDescriptorSet> descriptorWrites = std::vector<VkWriteDescriptorSet>{ uniformScene.GetWriteDescriptor(0, descriptorSet), m_uniformObject->GetWriteDescriptor(1, descriptorSet) }; // TODO: Modulaize this! , m_texture->GetWriteDescriptor(2, descriptorSet)
+
+		ShaderTerrains::UboObject uboObject = {};
+		uboObject.transform = Matrix4(*m_modelMatrix);
+		uboObject.shineDamper = 1.0f;
+		uboObject.reflectivity = 0.0f;
+		m_uniformObject->Update(&uboObject);
+
+		vkUpdateDescriptorSets(logicalDevice, static_cast<uint32_t>(descriptorWrites.size()), descriptorWrites.data(), 0, nullptr);
+
+		VkBuffer vertexBuffers[] = { m_model->GetVertexBuffer()->GetBuffer() };
+		VkDeviceSize offsets[] = { 0 };
+		VkDescriptorSet descriptors[] = { pipeline.GetDescriptorSet() };
+		vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline.GetPipelineLayout(), 0, 1, descriptors, 0, nullptr);
+		vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
+		vkCmdBindIndexBuffer(commandBuffer, m_model->GetIndexBuffer()->GetBuffer(), 0, m_model->GetIndexBuffer()->GetIndexType());
+		vkCmdDrawIndexed(commandBuffer, m_model->GetIndexBuffer()->GetIndexCount(), 1, 0, 0, 0);
+	}
+
 	void Terrain::GenerateMesh()
 	{
 		int count = VERTEX_COUNT * VERTEX_COUNT;
-		std::vector<float> vertices = std::vector<float>();
-		std::vector<float> textures = std::vector<float>();
-		std::vector<float> normals = std::vector<float>();
-		std::vector<float> colours = std::vector<float>();
-		std::vector<int> indices = std::vector<int>();
+		std::vector<Vertex> vertices = std::vector<Vertex>();
+		std::vector<uint16_t> indices = std::vector<uint16_t>();
 
-		Colour tint = Colour(Maths::RandomInRange(0.0f, 1.0f), Maths::RandomInRange(0.0f, 1.0f), 0.0f, 1.0f); // colour(0.0f, 0.8314f, 0.0f);
+		const Colour tint = Colour("#51ad5a");
 
 		for (int col = 0; col < VERTEX_COUNT; col++)
 		{
 			for (int row = 0; row < VERTEX_COUNT; row++)
 			{
-				Vector3 vertex = Vector3((row * SQUARE_SIZE) - (SIDE_LENGTH / 2.0f), 0.0f, (col * SQUARE_SIZE) - (SIDE_LENGTH / 2.0f));
-				vertex.m_y = GetHeight(vertex.m_x + m_position->m_x - (SIDE_LENGTH / 2.0f), vertex.m_z + m_position->m_z - (SIDE_LENGTH / 2.0f));
+				Vertex vertex = {};
+				vertex.position = Vector3((row * SQUARE_SIZE) - (SIDE_LENGTH / 2.0f), 0.0f, (col * SQUARE_SIZE) - (SIDE_LENGTH / 2.0f));
+				vertex.position.m_y = GetHeight(vertex.position.m_x + m_position->m_x - (SIDE_LENGTH / 2.0f), vertex.position.m_z + m_position->m_z - (SIDE_LENGTH / 2.0f));
+				vertex.textures = Vector2();
+				vertex.normal = CalculateNormal(vertex.position.m_x + m_position->m_x - (SIDE_LENGTH / 2.0f), vertex.position.m_z + m_position->m_z - (SIDE_LENGTH / 2.0f));
+				vertex.tangent = Vector3(tint.m_r, tint.m_g, tint.m_b);
 
-				Vector3 normal = CalculateNormal(vertex.m_x + m_position->m_x - (SIDE_LENGTH / 2.0f), vertex.m_z + m_position->m_z - (SIDE_LENGTH / 2.0f));
-				// colour tint = getBiomeColour(vertex.m_x + m_position->m_x, vertex.m_z + m_position->m_z);
-
-				vertices.push_back(vertex.m_x);
-				vertices.push_back(vertex.m_y);
-				vertices.push_back(vertex.m_z);
-
-				textures.push_back(0.0f);
-				textures.push_back(0.0f);
-
-				normals.push_back(normal.m_x);
-				normals.push_back(normal.m_y);
-				normals.push_back(normal.m_z);
-
-				colours.push_back(tint.m_r);
-				colours.push_back(tint.m_g);
-				colours.push_back(tint.m_b);
+				vertices.push_back(vertex);
 			}
 		}
 
@@ -98,7 +112,7 @@ namespace Flounder
 			}
 		}
 
-	//	m_model = new Model(indices, vertices, textures, normals, colours);
+		m_model = new Model(vertices, indices);
 
 		m_aabb->m_maxExtents->m_x = SIDE_LENGTH;
 		m_aabb->m_maxExtents->m_z = SIDE_LENGTH;
@@ -107,12 +121,10 @@ namespace Flounder
 		m_aabb->Update(*m_position, *m_rotation, 1.0f, m_aabb);
 
 		//	delete vertices;
-		//	delete normals;
-		//	delete colours;
 		//	delete indices;
 	}
 
-	void Terrain::StoreQuad1(std::vector<int> &indices, const int &topLeft, const int &topRight, const int &bottomLeft, const int &bottomRight, const bool &mixed)
+	void Terrain::StoreQuad1(std::vector<uint16_t> &indices, const int &topLeft, const int &topRight, const int &bottomLeft, const int &bottomRight, const bool &mixed)
 	{
 		indices.push_back(topLeft);
 		indices.push_back(bottomLeft);
@@ -122,7 +134,7 @@ namespace Flounder
 		indices.push_back(mixed ? bottomLeft : topLeft);
 	}
 
-	void Terrain::StoreQuad2(std::vector<int> &indices, const int &topLeft, const int &topRight, const int &bottomLeft, const int &bottomRight, const bool &mixed)
+	void Terrain::StoreQuad2(std::vector<uint16_t> &indices, const int &topLeft, const int &topRight, const int &bottomLeft, const int &bottomRight, const bool &mixed)
 	{
 		indices.push_back(topRight);
 		indices.push_back(topLeft);
@@ -144,169 +156,13 @@ namespace Flounder
 		return normal;
 	}
 
-	Colour Terrain::GetBiomeColour(const float &x, const float &z)
-	{
-		float height = GetHeight(x, z);
-		float moisture = GetFactorMoisture(x, height, z);
-
-		if (height <= 0.125f)
-		{
-			// Ocean.
-			return Colour(0.0824f, 0.3960f, 0.7530f);
-		}
-		if (height <= 0.25f)
-		{
-			if (moisture <= 0.16f)
-			{
-				// Subtropical Desert.
-				return Colour(0.914f, 0.8275f, 0.7804f);
-			}
-			if (moisture <= 0.33f)
-			{
-				// Grassland.
-				return Colour(0.7686f, 0.8314f, 0.6666f);
-			}
-			if (moisture <= 0.66f)
-			{
-				// Tropical Seasonal Forest.
-				return Colour(0.6627f, 0.8000f, 0.6431f);
-			}
-			if (moisture <= 1.0f)
-			{
-				// Tropical Rain Forest.
-				return Colour(0.6431f, 0.7686f, 0.6588f);
-			}
-		}
-		else if (height <= 0.5f)
-		{
-			if (moisture <= 0.16f)
-			{
-				// Temperate Desert.
-				return Colour(0.8941f, 0.9098f, 0.7922f);
-			}
-			if (moisture <= 0.5f)
-			{
-				// Grassland.
-				return Colour(0.7686f, 0.8314f, 0.6666f);
-			}
-			if (moisture <= 0.83f)
-			{
-				// Temperate Deciduous Forest.
-				return Colour(0.7059f, 0.7882f, 0.6627f);
-			}
-			if (moisture <= 1.0f)
-			{
-				// Temperate Rain Forest.
-				return Colour(0.6431f, 0.7686f, 0.6588f);
-			}
-		}
-		else if (height <= 0.75f)
-		{
-			if (moisture <= 0.33f)
-			{
-				// Temperate Desert.
-				return Colour(0.8941f, 0.9098f, 0.7922f);
-			}
-			if (moisture <= 0.66f)
-			{
-				// Shrubland.
-				return Colour(0.7686f, 0.8000f, 0.7333f);
-			}
-			if (moisture <= 1.0f)
-			{
-				// Taiga.
-				return Colour(0.8000f, 0.8314f, 0.7333f);
-			}
-		}
-		else if (height <= 1.0f)
-		{
-			if (moisture <= 0.16f)
-			{
-				// Scorched.
-				return Colour(0.6000f, 0.6000f, 0.6000f);
-			}
-			if (moisture <= 0.33f)
-			{
-				// Bare.
-				return Colour(0.7333f, 0.7333f, 0.7333f);
-			}
-			if (moisture <= 0.5f)
-			{
-				// Tundra.
-				return Colour(0.8666f, 0.8666f, 0.7333f);
-			}
-			if (moisture <= 1.0f)
-			{
-				// Snow.
-				return Colour(1.0000f, 1.0000f, 1.0000f);
-			}
-		}
-
-		return Colour(0.0824f, 0.3960f, 0.7530f);
-	}
-
-	float Terrain::GetFactorIsland(const float &x, const float &z)
-	{
-		const float worldSize = SIDE_LENGTH * 6;
-		const float worldIslandInside = 0.8f;
-		const float worldIslandOutside = 1.0f;
-		const float worldIslandParameter = 0.4f;
-
-		float circular = sqrt(pow(x, 2) + pow(z, 2)); // The current radius (circular map).
-		float rectangular = Maths::Max(fabs(x), fabs(z)); // The current radius (rectangular map).
-		float reading = ((1.0f - worldIslandParameter) * circular) + (worldIslandParameter * rectangular);
-
-		float radius1 = worldIslandInside * (worldSize / 2.0f); // The inside radius to the blur.
-		float radius2 = worldIslandOutside * (worldSize / 2.0f); // The outside radius to the blur.
-
-		if (x == 0.0f && z == 0.0f) // The special case where the reading is undefined.
-		{
-			return 1.0f;
-		}
-		if (reading > radius2) // If outside the upper bound there is no factor!
-		{
-			return 0.0f;
-		}
-		if (reading >= radius1) // Something between upper and lower, uses cos interpolation.
-		{
-			float blend = Maths::Clamp((reading - radius1) / (radius2 - radius1), 0.0f, 1.0f);
-			return Maths::Clamp(Maths::CosInterpolate(1.0f, 0.0f, blend), 0.0f, 1.0f);
-		}
-		// Fully inside of the lower radius, so full factor.
-		return 1.0f;
-	}
-
-	float Terrain::GetFactorMoisture(const float &x, const float &y, const float &z)
-	{
-		const float worldNoiseHeight = 40.0f;
-
-		// Calculate the moisture as a inverse of height with added noise.
-		float moisture = y / worldNoiseHeight;
-
-		// Set to 100% moisture in the ocean/lakes/rivers.
-		if ((y / worldNoiseHeight) <= 0.0f)
-		{
-			moisture = 1.0f;
-		}
-		else
-		{
-			moisture += Worlds::Get()->GetNoise()->GetNoise(x / 1.5f, z / 1.5f); // TODO
-		}
-
-		moisture = Maths::Clamp(moisture, 0.0f, 1.0f);
-		return moisture;
-	}
-
 	float Terrain::GetHeight(const float &x, const float &z)
 	{
 		const float worldNoiseSpread = 1.0f;
 		const float worldNoiseFrequency = 40.0f;
 		const float worldNoiseHeight = 40.0f;
-		const float dayNightCycle = 600.0f;
-		const float dayNightRatio = 0.7f;
 
 		float height = Worlds::Get()->GetNoise()->GetNoise(x / worldNoiseSpread, z / worldNoiseSpread, worldNoiseFrequency);
-		height *= GetFactorIsland(x, z);
 		height *= worldNoiseHeight;
 		return height;
 	}
--- Flounder.Demo/res/shaders/terrains/terrain.frag
+++ Flounder.Demo/res/shaders/terrains/terrain.frag
@@ -2,22 +2,29 @@
 
 #extension GL_ARB_separate_shader_objects : enable
 
-layout(binding = 1) uniform UBO 
+layout(binding = 1) uniform UboObject
 {
+	mat4 transform;
 	float shineDamper;
 	float reflectivity;
-} ubo;
+} object;
 
-layout(location = 0) flat in vec3 surfaceNormal;
-layout(location = 1) flat in vec3 surfaceColour;
+layout(location = 0) in vec3 fragmentNormal;
+layout(location = 1) in vec3 fragmentColour;
 
-layout(location = 0) out vec4 out_albedo;
-layout(location = 1) out vec4 out_normals;
-layout(location = 2) out vec4 out_extras;
+layout(location = 0) out vec4 outColour;
+//layout(location = 1) out vec4 outNormals;
+//layout(location = 2) out vec4 outExtras;
+
+const vec2 lightBias = vec2(0.7, 0.1);
+const vec3 lightDirection = vec3(0.2, -0.3, 0.8);
 
 void main(void) 
 {
-	out_albedo = vec4(surfaceColour, 1.0);
-	out_normals = vec4(surfaceNormal + 1.0 / 2.0, out_albedo.a);
-	out_extras = vec4(ubo.shineDamper, ubo.reflectivity, 0.0, out_albedo.a);
+	vec3 unitNormal = normalize(fragmentNormal);
+	float diffuseLight = max(dot(-lightDirection, unitNormal), 0.0) * lightBias.x + lightBias.y;
+
+	outColour = vec4(fragmentColour * diffuseLight, 1.0);
+//	outNormals = vec4(fragmentNormal + 1.0 / 2.0, out_albedo.a);
+//	outExtras = vec4(object.shineDamper, object.reflectivity, 0.0, out_albedo.a);
 }
