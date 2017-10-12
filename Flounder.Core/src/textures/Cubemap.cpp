b9722423b7ca94f60728a9df8069f0783e89ec6e
Matthew Albrecht <mattparks5855@gmail.com>
2017-10-12 06:46:39 -0500

    Tweaked cubemap.
    Former-commit-id: 88c3161c59e809da6373f54c883a541ed908e130

--- Flounder.Core/src/textures/Cubemap.cpp
+++ Flounder.Core/src/textures/Cubemap.cpp
@@ -15,6 +15,40 @@ namespace Flounder
 		m_layerCount(1),
 		m_imageSize((1024 * 1024 * 4) * 6)
 	{
+		stbi_uc* pix = (stbi_uc*)malloc(m_imageSize);
+		stbi_uc* offset = pix;
+		int width, height, components;
+
+		stbi_uc *pixelsFront = Texture::LoadPixels(filename + "Front" + fileExt, &width, &height, &components);
+		offset += m_front->GetSize();
+		std::memcpy(offset, pixelsFront, m_front->GetSize());
+
+		stbi_uc *pixelsBack = Texture::LoadPixels(filename + "Back" + fileExt, &width, &height, &components);
+		offset += m_front->GetSize();
+		std::memcpy(offset, pixelsBack, m_front->GetSize());
+
+		stbi_uc *pixelsLeft = Texture::LoadPixels(filename + "Left" + fileExt, &width, &height, &components);
+		offset += m_front->GetSize();
+		std::memcpy(offset, pixelsLeft, m_front->GetSize());
+
+		stbi_uc *pixelsRight = Texture::LoadPixels(filename + "Right" + fileExt, &width, &height, &components);
+		offset += m_front->GetSize();
+		std::memcpy(offset, pixelsRight, m_front->GetSize());
+
+		stbi_uc *pixelsTop = Texture::LoadPixels(filename + "Top" + fileExt, &width, &height, &components);
+		offset += m_front->GetSize();
+		std::memcpy(offset, pixelsTop, m_front->GetSize());
+
+		stbi_uc *pixelsBottom = Texture::LoadPixels(filename + "Bottom" + fileExt, &width, &height, &components);
+		offset += m_front->GetSize();
+		std::memcpy(offset, pixelsBottom, m_front->GetSize());
+
+		delete pixelsFront;
+		delete pixelsBack;
+		delete pixelsLeft;
+		delete pixelsRight;
+		delete pixelsTop;
+		delete pixelsBottom;
 	}
 
 	Cubemap::~Cubemap()
