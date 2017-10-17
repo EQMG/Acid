7d35aa3dc1926bd7c5382a64c339c306a927b28a
Matthew Albrecht <mattparks5855@gmail.com>
2017-10-17 14:51:09 -0500

    Dank Memes
    Former-commit-id: 422404106e4f28edea9e3a98202a6f98d7e423ab

--- Flounder.Demo/src/ManagerRender.cpp
+++ Flounder.Demo/src/ManagerRender.cpp
@@ -25,11 +25,11 @@ namespace Demo
 	{
 		const auto camera = Camera::Get()->GetCamera();
 
-		//	m_rendererSkyboxes->Render(commandBuffer, m_infinity, *camera);
+		m_rendererSkyboxes->Render(commandBuffer, m_infinity, *camera);
 		m_rendererWaters->Render(commandBuffer, m_infinity, *camera);
 		m_rendererTerrains->Render(commandBuffer, m_infinity, *camera);
-		m_rendererTest->Render(commandBuffer, m_infinity, *camera);
+		//m_rendererTest->Render(commandBuffer, m_infinity, *camera);
 
-		m_rendererGuis->Render(commandBuffer, m_infinity, *camera);
+		//m_rendererGuis->Render(commandBuffer, m_infinity, *camera);
 	}
 }
