10b86155df217983d74468da6f462cff3b05151b
Matthew Albrecht <mattparks5855@gmail.com>
2017-10-28 15:35:09 -0500

    Added some more UI tests.

--- Flounder.Demo/src/uis/OverlayDebug.cpp
+++ Flounder.Demo/src/uis/OverlayDebug.cpp
@@ -9,18 +9,24 @@ namespace Demo
 		//	m_textPosition(CreateStatus("POSITION: 0.0, 0.0, 0.0", 0.005f, 0.92f, AlignLeft)),
 		m_textFps(CreateStatus("FPS: 0", 0.005f, 0.95f, JustifyLeft)),
 		m_textUps(CreateStatus("UPS: 0", 0.005f, 0.98f, JustifyLeft)),
-		// m_textExample(new Text(this, Vector3(0.015f, 0.98f, false), Vector2(0.0f, 0.0f), "Kosmos", 1.25f, Uis::Get()->m_segoe, 0.7f, JustifyLeft)),
-		m_textExample(new Text(this, Vector3(0.5f, 0.5f, RelativeScreen), Vector2(0.0f, 0.0f), "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", 1.2f, Uis::Get()->m_proximanova->GetRegular(), 0.75f, JustifyFully)),
-		m_guiExample(new Gui(this, Vector3(0.5f, 1.0f, RelativeScreen), Vector3(1.0f, 1.0f, RelativeNone), Vector2(0.5f, 0.0f), new Texture("res/guis/geometry-grain.jpg"), 1)), // 0.625f
-		// m_guiExample(new Gui(this, Vector3(0.5f, 0.5f, RelativeScreen), Vector3(0.5f, 0.5f, RelativeScreen), Vector2(0.5f, 0.0f), new Texture("res/guis/geometry-grain.jpg"), 1)),
+		m_textExample(new Text(this, Vector3(0.5f, 0.5f, RelativeScreen), Vector2(0.5f, 0.0f), "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", 1.2f, Uis::Get()->m_proximanova->GetRegular(), 0.75f, JustifyCentre)),
+		m_guiExample(new Gui(this, Vector3(0.5f, 0.5f, RelativeScreen), Vector3(0.5f, 0.5f, RelativeScreen), Vector2(0.5f, 0.5f), new Texture("res/guis/geometry-grain.jpg"), 1)),
 		m_timerUpdate(new Timer(0.333f))
 	{
-#ifndef FLOUNDER_CONFIG_RELEASE
-		m_textExample->SetTextColour(Colour("#ffffff"));
+		m_guiExample->SetColourOffset(Colour("#553982"));
 
-		/*Texture *textureInv = new Texture("res/guis/geometry-grain.jpg");
+		Texture *textureInv = new Texture("res/guis/geometry-grain.jpg");
 
-		for (int i = 0; i < 6; i++)
+		Text *text1 = new Text(this, Vector3(0.015f, 0.985f, false), Vector2(0.0f, 0.0f), "Kosmos", 2.5f, Uis::Get()->m_proximanova->GetBold(), 0.7f, JustifyLeft);
+		Text *text2 = new Text(this, Vector3(0.019f, 0.91f, false), Vector2(0.0f, 0.0f), "Created By: Equilibrium Games", 0.6f, Uis::Get()->m_proximanova->GetLight(), 0.7f, JustifyLeft);
+		Text *text3 = new Text(this, Vector3(0.4f, 0.947f, false), Vector2(0.0f, 0.0f), "New Game", 0.9f, Uis::Get()->m_proximanova->GetRegular(), 0.7f, JustifyLeft);
+		Gui *gui1 = new Gui(this, Vector3(0.5f, 1.0f, RelativeScreen), Vector3(1.0f, 1.0f, RelativeNone), Vector2(0.5f, 0.0f), new Texture("res/guis/geometry-grain.jpg"), 1);
+		gui1->SetScissor(Vector4(0.0f, 0.875f, 1.0f, 1.0f));
+		gui1->SetColourOffset(Colour("#553982"));
+		Gui *gui2 = new Gui(this, Vector3(0.4f, 0.875f, false), Vector3(0.125f, 0.01f, true), Vector2(0.0f, 1.0f), textureInv, 1);
+		gui2->SetColourOffset(Colour("#386AB5"));
+
+		/*for (int i = 0; i < 6; i++)
 		{
 			for (int j = 0; j < 6; j++)
 			{
@@ -33,13 +39,10 @@ namespace Demo
 				gui->SetColourOffset(Colour("#D14841"));
 			}
 		}*/
-
-		m_guiExample->SetScissor(Vector4(0.0f, 0.875f, 1.0f, 1.0f));
-		m_guiExample->SetColourOffset(Colour("#553982"));
-#else
-		m_textExample->SetVisible(false);
-		m_guiExample->SetVisible(false);
-#endif
+//#ifdef FLOUNDER_CONFIG_RELEASE
+//		m_textExample->SetVisible(false);
+//		m_guiExample->SetVisible(false);
+//#endif
 	}
 
 	OverlayDebug::~OverlayDebug()
