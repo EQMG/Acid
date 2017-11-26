880692ef60310d95431aad037bab977a40c7cf5d
Matthew Albrecht <mattparks5855@gmail.com>
2017-11-26 14:11:19 -0600

    Work on better lighting.
    Former-commit-id: 17cd00bdaa4cea868f50c7de0d274289e094c3e0

--- Flounder.Core/src/lights/Light.cpp
+++ Flounder.Core/src/lights/Light.cpp
@@ -4,29 +4,22 @@ namespace Flounder
 {
 	Light::Light() :
 		m_colour(new Colour(1.0f, 1.0f, 1.0f)),
-		m_position(new Vector3()),
-		m_attenuation(new Attenuation())
+		m_radius(-1.0f),
+		m_position(new Vector3())
 	{
 	}
 
-	Light::Light(const Colour &colour, const Vector3 &position) :
+	Light::Light(const Colour &colour, const float &radius, const Vector3 &position) :
 		m_colour(new Colour(colour)),
-		m_position(new Vector3(position)),
-		m_attenuation(new Attenuation())
-	{
-	}
-
-	Light::Light(const Colour &colour, const Vector3 &position, const Attenuation &attenuation) :
-		m_colour(new Colour(colour)),
-		m_position(new Vector3(position)),
-		m_attenuation(new Attenuation(attenuation))
+		m_radius(radius),
+		m_position(new Vector3(position))
 	{
 	}
 
 	Light::Light(const Light &source) :
 		m_colour(new Colour(*source.m_colour)),
-		m_position(new Vector3(*source.m_position)),
-		m_attenuation(new Attenuation(*source.m_attenuation))
+		m_radius(source.m_radius),
+		m_position(new Vector3(*source.m_position))
 	{
 	}
 
@@ -34,7 +27,6 @@ namespace Flounder
 	{
 		delete m_colour;
 		delete m_position;
-		delete m_attenuation;
 	}
 
 	Light *Light::Set(const Colour &colour, const Vector3 &position)
@@ -44,19 +36,19 @@ namespace Flounder
 		return this;
 	}
 
-	Light *Light::Set(const Colour &colour, const Vector3 &position, const Attenuation &attenuation)
+	Light *Light::Set(const Colour &colour, const float &radius, const Vector3 &position)
 	{
 		m_colour->Set(colour);
+		m_radius = radius;
 		m_position->Set(position);
-		m_attenuation->Set(attenuation);
 		return this;
 	}
 
 	Light *Light::Set(const Light &source)
 	{
 		m_colour->Set(*source.m_colour);
+		m_radius = source.m_radius;
 		m_position->Set(*source.m_position);
-		m_attenuation->Set(*source.m_attenuation);
 		return this;
 	}
 }
