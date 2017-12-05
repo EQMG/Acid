c49ebb33395c34d4c3b54fe528280e6912cd5147
Matthew Albrecht <mattparks5855@gmail.com>
2017-12-04 21:50:52 -0600

    Reworked includes into submodules, reworked cmake
    Former-commit-id: d59df88dbf17b18617c700ff727dcbf63447d666
    Former-commit-id: 802ce39c9b721527a7c67b2d967eec4af2fd4625

--- Scripts/build_shaders.bat
+++ Scripts/build_shaders.bat
@@ -1,16 +1,16 @@
-for /R "C:\Users\mattp\Documents\Flounder" %%G in (*.frag) do ( 
+for /R "C:\Users\mattp\Documents\Flounder\Sources" %%G in (*.frag) do ( 
 	C:\VulkanSDK\1.0.65.0\Bin32\glslangValidator.exe -V %%G -o %%G.spv
 )
 
-for /R "C:\Users\mattp\Documents\Flounder" %%G in (*.vert) do ( 
+for /R "C:\Users\mattp\Documents\Flounder\Sources" %%G in (*.vert) do ( 
 	C:\VulkanSDK\1.0.65.0\Bin32\glslangValidator.exe -V %%G -o %%G.spv
 )
 
-for /R "C:\Users\mattp\Documents\Flounder" %%G in (*.geom) do ( 
+for /R "C:\Users\mattp\Documents\Flounder\Sources" %%G in (*.geom) do ( 
 	C:\VulkanSDK\1.0.65.0\Bin32\glslangValidator.exe -V %%G -o %%G.spv
 )
 
-for /R "C:\Users\mattp\Documents\Flounder" %%G in (*.comp) do ( 
+for /R "C:\Users\mattp\Documents\Flounder\Sources" %%G in (*.comp) do ( 
 	C:\VulkanSDK\1.0.65.0\Bin32\glslangValidator.exe -V %%G -o %%G.spv
 )
 
