fcd2d04ca018bc99d33e3c846ab671604cb96b84
Matthew Albrecht <mattparks5855@gmail.com>
2017-12-06 22:24:11 -0600

    Some VS build fixes, not fully fixed

--- Sources/FlounderEditor/Main.cpp
+++ Sources/FlounderEditor/Main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <Windows.h>
 
-int main()
+#if (FLOUNDER_CONFIG_RELEASE && FLOUNDER_PLATFORM_WINDOWS)
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
+#else
+int main(int argc, char **argv)
+#endif
 {
 	std::cout << "Hello World" << std::endl;
 	std::cin.get();
+	return 0;
 }
