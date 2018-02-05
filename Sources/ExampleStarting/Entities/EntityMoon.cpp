45fe77555d0fa8adb9a0ba26e6ee47a9c2988111
Matthew Albrecht <mattparks5855@gmail.com>
2018-02-05 06:59:55 -0600

    Entity prefab work.
    Former-commit-id: 21cc9123929102338fd1a5327138ec427eb49e96
    Former-commit-id: 1c9a457817da9d7e2cf244d26c8bb2b25e63f0f2

--- Sources/ExampleStarting/Entities/EntityMoon.cpp
+++ Sources/ExampleStarting/Entities/EntityMoon.cpp
@@ -9,14 +9,14 @@
 #include <Models/Shapes/Sphere.hpp>
 
 EntityMoon::EntityMoon(ISpatialStructure<Entity *> *structure, const Transform &transform) :
-	Entity(structure, transform)
+	Entity(transform, structure)
 {
 	AddComponent(new ComponentModel(
 		Sphere::Resource(16, 16, 1.0f),
 		Texture::Resource("Resources/Entities/Moon/Diffuse.png")
 	));
 	AddComponent(new ComponentMaterial(0.5f, 0.5f, false, true, true));
-	AddComponent(new ComponentGlow(Texture::Resource("Resources/Entities/Sun/Glow.png")));
+	AddComponent(new ComponentGlow(Texture::Resource("Resources/Entities/Moon/Glow.png")));
 	AddComponent(new ComponentLight(Light(Colour("#1E1E1E"), -1.0f), Vector3()));
 	AddComponent(new ComponentCelestial(CelestialMoon));
 //	AddComponent(new ComponentCollider());
