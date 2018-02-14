c7720003ac67f56c577069730a739aab6d819792
Matthew Albrecht <mattparks5855@gmail.com>
2018-02-14 06:58:52 -0600

    Fixed a shared libs build error
    Former-commit-id: 856a740f73e1cd7993f11a4908bb6ccf4558393a
    Former-commit-id: 9ff717c81aaf9fb64ea356d48b585a0371da33ca

--- Scripts/generate_vs.bat
+++ Scripts/generate_vs.bat
@@ -1,6 +1,6 @@
 cd ../
 mkdir Build
 cd Build
-cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release
+cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
 cmake --build .
 pause
