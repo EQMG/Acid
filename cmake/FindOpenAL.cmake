656c42820fb55b4c03085c3ceac570132489b46b
Matthew Albrecht <mattparks5855@gmail.com>
2018-02-14 08:38:53 -0600

    Moved Find functions into cmake folder
    Former-commit-id: 7ca665c4e22b18140e1e28e7b1bb7f2877fb4ed2
    Former-commit-id: addd07d6a049781844f6ccf198c623c98893b731

---
+++ cmake/FindOpenAL.cmake
@@ -0,0 +1,15 @@
+if(APPLE)
+	FIND_PATH(OPENAL_INCLUDES al.h)
+else()
+	set(OPENAL_INCLUDES "${PROJECT_SOURCE_DIR}/Libraries/openal/include")
+endif()
+
+find_library(OPENAL_LIBRARY NAMES OpenAL32 OpenAL openal PATHS "${PROJECT_SOURCE_DIR}/Libraries/openal/lib/")
+
+list(APPEND LIBRARIES_INCLUDES ${OPENAL_INCLUDES} )
+list(APPEND LIBRARIES_LINKS "${OPENAL_LIBRARY}")
+message(STATUS "OpenAL: ${OPENAL_LIBRARY}")
+
+if(NOT OPENAL_LIBRARY)
+	message(FATAL_ERROR "OpenAL library not found!")
+endif()
