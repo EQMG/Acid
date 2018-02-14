f6aff45541e001a7752498e968bc29f1e6e48957
Matthew Albrecht <mattparks5855@gmail.com>
2018-02-14 08:38:53 -0600

    Moved Find functions into cmake folder
    Former-commit-id: 7ca665c4e22b18140e1e28e7b1bb7f2877fb4ed2
    Former-commit-id: addd07d6a049781844f6ccf198c623c98893b731

---
+++ cmake/FindVulkan.cmake
@@ -0,0 +1,41 @@
+if(APPLE)
+	find_path(METAL_INCLUDES Metal.h)
+	find_library(METAL_LIBRARY NAMES Metal)
+
+	if(NOT METAL_LIBRARY)
+		message(FATAL_ERROR "Metal library not found!")
+	endif()
+
+	find_path(IOSURFACE_INCLUDES IOSurface.h)
+	find_library(IOSURFACE_LIBRARY NAMES IOSurface)
+
+	if(NOT IOSURFACE_LIBRARY)
+		message(FATAL_ERROR "IOSurface library not found!")
+	endif()
+
+	find_path(QUARTZCORE_INCLUDES QuartzCore.h)
+	find_library(QUARTZCORE_LIBRARY NAMES QuartzCore)
+
+	if(NOT QUARTZCORE_LIBRARY)
+		message(FATAL_ERROR "QuartzCore library not found!")
+	endif()
+
+	list(APPEND LIBRARIES_INCLUDES ${METAL_INCLUDES} ${IOSURFACE_INCLUDES} ${QUARTZCORE_INCLUDES})
+	list(APPEND LIBRARIES_LINKS "${METAL_LIBRARY}" "${IOSURFACE_LIBRARY}" "${QUARTZCORE_LIBRARY}")
+
+	set(VULKAN_INCLUDES "${PROJECT_SOURCE_DIR}/Libraries/moltenvk/include")
+	find_library(VULKAN_LIBRARY NAMES MoltenVK PATHS "${PROJECT_SOURCE_DIR}/Libraries/moltenvk/lib/")
+else()
+	set(VULKAN_INCLUDES "${PROJECT_SOURCE_DIR}/Libraries/vulkan/include")
+	find_library(VULKAN_LIBRARY NAMES vulkan-1 vulkan PATHS "${PROJECT_SOURCE_DIR}/Libraries/vulkan/lib/")
+endif()
+
+set(VULKAN_INCLUDE_DIR ${VULKAN_INCLUDES})
+
+if(NOT VULKAN_LIBRARY)
+	message(FATAL_ERROR "Vulkan library not found!")
+endif()
+
+list(APPEND LIBRARIES_INCLUDES ${VULKAN_INCLUDES})
+list(APPEND LIBRARIES_LINKS "${VULKAN_LIBRARY}")
+message(STATUS "Vulkan: ${VULKAN_LIBRARY}")
