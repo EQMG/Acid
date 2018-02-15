c04ed80ebae3bd1141cea0bf23ff9800005e9523
Matthew Albrecht <mattparks5855@gmail.com>
2018-02-15 08:40:56 -0600

    Build shared by default, removed Windows warnings

--- Scripts/generate_vs.bat
+++ Scripts/generate_vs.bat
@@ -1,6 +1,6 @@
 cd ../
 mkdir Build
 cd Build
-cmake .. -G "Visual Studio 15 2017 Win64" -DBUILD_SHARED_LIBS:BOOL=OFF -DCMAKE_BUILD_TYPE=Release
+cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 pause
