2f9ce6db18f51204df61d87ad19f880e295d639e
Matthew Albrecht <mattparks5855@gmail.com>
2018-02-17 07:48:56 -0600

    Bullet build fixes
    Former-commit-id: b490c0cbb5173841396375cd00f9936b344a95f1
    Former-commit-id: 10b92a210262eb33f6633e3e7af0e8d1d6c62602

--- Libraries/FindBullet.cmake
+++ Libraries/FindBullet.cmake
@@ -5,9 +5,10 @@ set(USE_GRAPHICAL_BENCHMARK OFF CACHE INTERNAL "Use Graphical Benchmark")
 set(BUILD_OPENGL3_DEMOS OFF CACHE INTERNAL "Set when you want to build the OpenGL3+ demos")
 set(BUILD_EXTRAS OFF CACHE INTERNAL "Set when you want to build the extras")
 set(USE_GLUT OFF CACHE INTERNAL "Use Glut")
+set(BUILD_UNIT_TESTS OFF CACHE INTERNAL "Build Unit Tests")
 add_subdirectory(${PROJECT_SOURCE_DIR}/Libraries/bullet3)
 set(CMAKE_DEBUG_POSTFIX "" CACHE STRING "Adds a postfix for debug-built libraries.")
-set(BULLET_LIBRARY "BULLET_PHYSICS")
+set(BULLET_LIBRARY "bullet")
 
 message(WARNING "${BULLET_LIBRARY}")
 
