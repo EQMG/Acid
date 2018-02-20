de9b2a67858e462aa40304fd7d1d62e4ba91a2fc
Matthew Albrecht <mattparks5855@gmail.com>
2018-02-20 17:49:35 -0600

    Component/GameObject abstractions

---
+++ Sources/FlounderEngine/Objects/Behaviour.cpp
@@ -0,0 +1,28 @@
+#include "Behaviour.hpp"
+
+namespace Flounder
+{
+	Behaviour::Behaviour()
+	{
+	}
+
+	Behaviour::~Behaviour()
+	{
+	}
+
+	void Behaviour::Start()
+	{
+	}
+
+	void Behaviour::Update()
+	{
+	}
+
+	void Behaviour::OnEnable()
+	{
+	}
+
+	void Behaviour::OnDisable()
+	{
+	}
+}
