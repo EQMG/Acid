28b85937a3c97e148c79d7f6fb6f23d01c5a43ba
Matthew Albrecht <mattparks5855@gmail.com>
2018-02-20 17:49:35 -0600

    Component/GameObject abstractions
    Former-commit-id: de9b2a67858e462aa40304fd7d1d62e4ba91a2fc
    Former-commit-id: 590dcb9412626834d314420023de7469834add3d

---
+++ Sources/FlounderEngine/Objects/Component.cpp
@@ -0,0 +1,5 @@
+#include "Component.hpp"
+
+namespace Flounder
+{
+}
---
+++ Sources/FlounderEngine/Objects/Component.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../Prerequisites.hpp"
+
+namespace Flounder
+{
+	class F_EXPORT Component
+	{
+
+	};
+}
