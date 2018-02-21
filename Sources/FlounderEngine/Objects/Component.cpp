b2b2ec748c693e70d7250e7c6d530bebbab6fb57
Matthew Albrecht <mattparks5855@gmail.com>
2018-02-21 06:59:23 -0600

    Component data sets.
    Former-commit-id: acabe77063af2c8cd57013029c8985eb56ccf881
    Former-commit-id: 8b4331ccf4cb5eb1ae9c4766a932565a6bad4b57

--- Sources/FlounderEngine/Objects/Component.cpp
+++ Sources/FlounderEngine/Objects/Component.cpp
@@ -6,12 +6,32 @@ namespace Flounder
 {
 	Component::Component() :
 		m_name(""),
-		m_gameObject(nullptr)
+		m_gameObject(nullptr),
+		m_values(new std::map<std::string, ComponentGetSet>())
 	{
+	//	Link<std::string>("Name", LINK_GET(std::string, GetName()), LINK_SET(std::string, SetName(v)));
 	}
 
 	Component::~Component()
 	{
-	//	m_gameObject->RemoveComponent(this);
+		delete m_values;
+	}
+
+	void Component::Load(ComponentPrefab *componentPrefab)
+	{
+		m_values->clear();
+
+		for (const auto data : componentPrefab->GetData())
+		{
+
+		}
+	}
+
+	void Component::Write(ComponentPrefab *componentPrefab)
+	{
+		for (const auto value : *m_values)
+		{
+
+		}
 	}
 }
