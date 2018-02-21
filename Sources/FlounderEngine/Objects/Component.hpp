fb76a2040dfee63a8fcd72dee631745eed32b0dc
Matthew Albrecht <mattparks5855@gmail.com>
2018-02-21 06:59:23 -0600

    Component data sets.
    Former-commit-id: acabe77063af2c8cd57013029c8985eb56ccf881
    Former-commit-id: 8b4331ccf4cb5eb1ae9c4766a932565a6bad4b57

--- Sources/FlounderEngine/Objects/Component.hpp
+++ Sources/FlounderEngine/Objects/Component.hpp
@@ -1,26 +1,66 @@
 #pragma once
 
 #include <string>
+#include <functional>
+#include <map>
 #include "../Prerequisites.hpp"
+#include "Prefabs/ComponentPrefab.hpp"
 
 namespace Flounder
 {
 	class GameObject;
 
+	struct ComponentGetSet
+	{
+		std::string dataType;
+		std::function<std::string()> getter;
+		std::function<void(std::string)> setter;
+	};
+
 	class F_EXPORT Component
 	{
-	private:
+	protected:
 		std::string m_name;
 		GameObject *m_gameObject;
+	private:
+		std::map<std::string, ComponentGetSet> *m_values;
 	public:
+#define LINK_GET(t, f) [&]() -> t { return f; }
+#define LINK_SET(t, f) [&](const t &v) -> void { f; }
+
 		Component();
 
 		virtual ~Component();
 
-		virtual std::string GetName() = 0;
+		virtual std::string GetName() const = 0;
+
+		void SetName(const std::string &name) { m_name = name; }
 
 		GameObject *GetGameObject() const { return m_gameObject; }
 
 		void SetGameObject(GameObject *gameObject) { m_gameObject = gameObject; }
+
+		void Load(ComponentPrefab *componentPrefab);
+
+		void Write(ComponentPrefab *componentPrefab);
+	protected:
+		template<typename T>
+		void Link(const std::string &name, const std::function<T()> &getter, const std::function<void(T)> &setter = [](const T &v) -> void { })
+		{
+			ComponentGetSet componentGetSet{};
+			componentGetSet.dataType = "Float_Slider";
+
+			if (getter != nullptr)
+			{
+				componentGetSet.getter = [&]() -> std::string { return std::to_string(getter()); };
+			}
+
+			if (setter != nullptr)
+			{
+				componentGetSet.setter = [&](std::string value) -> void { setter(value); };
+			}
+
+			m_values->insert(std::make_pair(name, componentGetSet));
+		}
 	};
 }
