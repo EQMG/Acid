eac20318f3edaa5d65ded0b4d256ed0a8ee50e38
Matthew Albrecht <mattparks5855@gmail.com>
2018-03-10 18:36:57 -0600

    Fixed building shaders, fixed CMake
    Former-commit-id: dbe6672120c018e41222e7a12f899786422ab783
    Former-commit-id: a1cfbe651c7d8c538012a7c31aadbd5fd6c0c423

--- Scripts/build_shaders.bat
+++ Scripts/build_shaders.bat
@@ -1,17 +1,18 @@
+pushd "%~dp0"
 cd ../
 
 for /R "Resources" %%G in (*.frag) do ( 
-	Libraries\glslangValidator.exe -V %%G -o %%G.spv
+	Libraries\glslangValidator.exe -V "%%G" -o "%%G.spv"
 )
 
 for /R "Resources" %%G in (*.vert) do ( 
-	Libraries\glslangValidator.exe -V %%G -o %%G.spv
+	Libraries\glslangValidator.exe -V "%%G" -o "%%G.spv"
 )
 
 for /R "Resources" %%G in (*.geom) do ( 
-	Libraries\glslangValidator.exe -V %%G -o %%G.spv
+	Libraries\glslangValidator.exe -V "%%G" -o "%%G.spv"
 )
 
 for /R "Resources" %%G in (*.comp) do ( 
-	Libraries\glslangValidator.exe -V %%G -o %%G.spv
+	Libraries\glslangValidator.exe -V "%%G" -o "%%G.spv"
 )
