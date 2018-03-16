4bc98dcdd462fe1cdb316fea3e40fce132c478c0
Matthew Albrecht <mattparks5855@gmail.com>
2018-03-15 20:06:35 -0500

    Voxel chunk structure changes
    Former-commit-id: 17af721f793f649e74eb0e75c66943cd1c2f04b0
    Former-commit-id: 6d85d4147c605a01cea243c772dbdbf6ca37ab49

--- Sources/FlounderEngine/Voxels/BlockFace.hpp
+++ Sources/FlounderEngine/Voxels/BlockFace.hpp
@@ -5,34 +5,30 @@
 
 namespace Flounder
 {
-	enum BlockFaceType
+	enum FaceType
 	{
-		TypeNull = 0,
-		TypeFront = 1,
-		TypeBack = 2,
-		TypeTop = 3,
-		TypeBottom = 4,
-		TypeLeft = 5,
-		TypeRight = 6
+		FaceNull = 0,
+		FaceFront = 1,
+		FaceBack = 2,
+		FaceTop = 3,
+		FaceBottom = 4,
+		FaceLeft = 5,
+		FaceRight = 6
 	};
 
 	class BlockFace
 	{
 	private:
-		BlockFaceType m_faceType;
+		FaceType m_faceType;
 		short m_blockType;
-		Vector3 m_position;
-		Vector3 m_scale;
 	public:
-		BlockFace(const BlockFaceType &faceType, const Vector3 &position, const Vector3 &scale, const short &blockType);
+		BlockFace(const FaceType &faceType, const short &blockType);
 
-		BlockFaceType GetFaceType() const { return m_faceType; }
+		FaceType GetFaceType() const { return m_faceType; }
 
-		void AppendVertices(std::vector<Vertex> *vertices) const;
+		void AppendVertices(std::vector<Vertex> *vertices, const Vector3 &position, const Vector3 &scale) const;
 
 		void AppendIndices(std::vector<uint32_t> *indices, unsigned int indexStart) const;
-
-		static bool CompareFaces(const BlockFace &face1, const BlockFace &face2);
 
 		bool operator==(const BlockFace &other) const;
 
