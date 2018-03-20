56ee5344d1b063cb9f7ad5e556fd703d4b7abc0c
Matthew Albrecht <mattparks5855@gmail.com>
2018-03-20 08:49:40 -0500

    More animation loading.
    Former-commit-id: 7abc1fd4f8cc5764834dc1cc33017eb399fc1ae9
    Former-commit-id: bad172221bc24fbc0823fdab033c6e97f1f07118

--- Sources/FlounderEngine/Meshes/Animations/Skeleton/SkeletonLoader.cpp
+++ Sources/FlounderEngine/Meshes/Animations/Skeleton/SkeletonLoader.cpp
@@ -24,7 +24,7 @@ namespace Flounder
 
 	JointData *SkeletonLoader::LoadJointData(LoadedValue *jointNode, const bool &isRoot)
 	{
-		JointData joint = ExtractMainJointData(jointNode, isRoot);
+		JointData *joint = ExtractMainJointData(jointNode, isRoot);
 
 		if (jointNode->GetChild("node"))
 		{
@@ -33,20 +33,20 @@ namespace Flounder
 				for (auto childNode : jointNode->GetChild("node")->m_children)
 				{
 					JointData *childJoint = LoadJointData(childNode, false);
-					joint.AddChild(childJoint);
+					joint->AddChild(childJoint);
 				}
 			}
 			else if (!jointNode->GetChild("node")->m_children.empty())
 			{
 				JointData *childJoint = LoadJointData(jointNode->GetChild("node"), false);
-				joint.AddChild(childJoint);
+				joint->AddChild(childJoint);
 			}
 		}
 
-		return &joint;
+		return joint;
 	}
 
-	JointData SkeletonLoader::ExtractMainJointData(LoadedValue *jointNode, const bool &isRoot)
+	JointData *SkeletonLoader::ExtractMainJointData(LoadedValue *jointNode, const bool &isRoot)
 	{
 		std::string nameId = jointNode->GetChild("-id")->GetString();
 		auto index = GetBoneIndex(nameId);
@@ -61,7 +61,7 @@ namespace Flounder
 		}
 
 		m_jointCount++;
-		return JointData(index, nameId, matrix);
+		return new JointData(index, nameId, matrix);
 	}
 
 	int SkeletonLoader::GetBoneIndex(const std::string &name)
