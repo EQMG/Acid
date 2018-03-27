4527d01ebd58c4ae661f6609fd6d67fd27b77f89
Matthew Albrecht <mattparks5855@gmail.com>
2018-03-27 08:42:56 -0500

    Just using glslang over shaderc
    Former-commit-id: fc16b367786d82360c77793ddbfc8629915b76cf
    Former-commit-id: b26559fd83b22e83f9f1803032774f25d7e4f754

---
+++ Libraries/FindGlslang.cmake
@@ -0,0 +1,16 @@
+set(GLSLANG_INCLUDES "${PROJECT_SOURCE_DIR}/Libraries/glslang/glslang/Include" "${PROJECT_SOURCE_DIR}/Libraries/glslang/SPIRV")
+set(SKIP_GLSLANG_INSTALL ON CACHE INTERNAL "Skip installation")
+set(ENABLE_AMD_EXTENSIONS OFF CACHE INTERNAL "Enables support of AMD-specific extensions")
+set(ENABLE_GLSLANG_BINARIES OFF CACHE INTERNAL "Builds glslangValidator and spirv-remap")
+set(ENABLE_NV_EXTENSIONS OFF CACHE INTERNAL "Enables support of Nvidia-specific extensions")
+set(ENABLE_HLSL OFF CACHE INTERNAL "Enables HLSL input support")
+set(ENABLE_OPT OFF CACHE INTERNAL "Enables spirv-opt capability if present")
+add_subdirectory(${PROJECT_SOURCE_DIR}/Libraries/glslang)
+set(GLSLANG_LIBRARY "glslang" "SPIRV")
+
+set(LIBRARIES_INCLUDES ${LIBRARIES_INCLUDES} ${GLSLANG_INCLUDES})
+set(LIBRARIES_LINKS ${LIBRARIES_LINKS} ${GLSLANG_LIBRARY})
+
+if(NOT GLSLANG_LIBRARY)
+    message(FATAL_ERROR "Glslang library not found!")
+endif()
