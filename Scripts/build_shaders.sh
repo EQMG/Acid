c330605aea2aa4056cc972cd879f3a8e18abb22d
Matthew Albrecht <mattparks5855@gmail.com>
2018-03-27 22:30:44 -0500

    More glslang progress

--- Scripts/build_shaders.sh
+++
@@ -1,7 +0,0 @@
-cd ../
-
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-find ${SCRIPTPATH}/Resources/Shaders -type f -iname "*.frag" -exec ${SCRIPTPATH}/Libraries/glslangValidator -V {} -o {}.spv \;
-find ${SCRIPTPATH}/Resources/Shaders -type f -iname "*.vert" -exec ${SCRIPTPATH}/Libraries/glslangValidator -V {} -o {}.spv \;
-find ${SCRIPTPATH}/Resources/Shaders -type f -iname "*.geom" -exec ${SCRIPTPATH}/Libraries/glslangValidator -V {} -o {}.spv \;
-find ${SCRIPTPATH}/Resources/Shaders -type f -iname "*.comp" -exec ${SCRIPTPATH}/Libraries/glslangValidator -V {} -o {}.spv \;
