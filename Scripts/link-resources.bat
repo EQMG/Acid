f5fc9fd5dc10ee54b2c32ea2a9b73a941bb640b3
Matthew Albrecht <mattparks5855@gmail.com>
2018-05-03 14:22:38 -0500

    Readme and build update to v0.9.

--- Scripts/link-resources.bat
+++ Scripts/link-resources.bat
@@ -4,4 +4,5 @@ mklink /J Build\Debug\bin\Resources Resources
 mklink /J Build\Release\bin\Resources Resources
 mklink /J Build\RelWithDebInfo\bin\Resources Resources
 
-mklink /J Build\Sources\ExampleStarting\Resources Resources
+mklink /J Build\Tests\TestDeferred\Resources Resources
+mklink /J Build\Tests\TestGuis\Resources Resources
