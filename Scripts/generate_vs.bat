9981f2445800427c2fc32d658ac59f26e89313d6
Matthew Albrecht <mattparks5855@gmail.com>
2018-05-10 16:10:19 -0500

    Testing library build.

--- Scripts/generate_vs.bat
+++ Scripts/generate_vs.bat
@@ -1,6 +1,6 @@
 cd ../
 mkdir Build
 cd Build
-cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF
-cmake --build .
+cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DFLOUNDER_BUILD_TESTING=OFF
+cmake --build . --target install
 pause
