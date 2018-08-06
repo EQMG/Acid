b3c844a772026acdabf045a9cdd4ae44b3dfba62
Matthew Albrecht <mattparks5855@gmail.com>
2018-08-06 01:46:33 -0500

    Fixed texture pixels that have been changed on the GPU.

--- Sources/Renderer/Screenshot/Screenshot.hpp
+++ Sources/Renderer/Screenshot/Screenshot.hpp
@@ -13,10 +13,5 @@ namespace acid
 		/// </summary>
 		///	<param name="filename"> The new width in pixels. </param>
 		static void Capture(const std::string &filename);
-
-	private:
-		static void InsertImageMemoryBarrier(const VkCommandBuffer &cmdbuffer, const VkImage &image, const VkAccessFlags &srcAccessMask,
-											 const VkAccessFlags &dstAccessMask, const VkImageLayout &oldImageLayout, const VkImageLayout &newImageLayout,
-											 const VkPipelineStageFlags &srcStageMask, const VkPipelineStageFlags &dstStageMask, const VkImageSubresourceRange &subresourceRange);
 	};
 }
--- Sources/Textures/Texture.cpp
+++ Sources/Textures/Texture.cpp
@@ -178,22 +178,29 @@ namespace acid
 	{
 		auto logicalDevice = Display::Get()->GetVkLogicalDevice();
 
+		VkImage dstImage;
+		VkDeviceMemory dstImageMemory;
+		Texture::CopyImage(m_image, dstImage, dstImageMemory, m_width, m_height);
+
 		VkImageSubresource imageSubresource = {};
 		imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
 		imageSubresource.mipLevel = 0;
 		imageSubresource.arrayLayer = 0;
 
 		VkSubresourceLayout subresourceLayout;
-		vkGetImageSubresourceLayout(logicalDevice, m_image, &imageSubresource, &subresourceLayout);
+		vkGetImageSubresourceLayout(logicalDevice, dstImage, &imageSubresource, &subresourceLayout);
 
-		uint8_t *pixels = new uint8_t[subresourceLayout.size];
+		uint8_t *result = new uint8_t[subresourceLayout.size];
 
 		void *data;
-		vkMapMemory(logicalDevice, m_bufferMemory, subresourceLayout.offset, subresourceLayout.size, 0, &data);
-		memcpy(pixels, data, static_cast<size_t>(subresourceLayout.size));
-		vkUnmapMemory(logicalDevice, m_bufferMemory);
+		vkMapMemory(logicalDevice, dstImageMemory, subresourceLayout.offset, subresourceLayout.size, 0, &data);
+		memcpy(result, data, static_cast<size_t>(subresourceLayout.size));
+		vkUnmapMemory(logicalDevice, dstImageMemory);
 
-		return pixels;
+		vkFreeMemory(logicalDevice, dstImageMemory, nullptr);
+		vkDestroyImage(logicalDevice, dstImage, nullptr);
+
+		return result;
 	}
 
 	VkDeviceSize Texture::LoadSize(const std::string &filepath)
@@ -402,11 +409,6 @@ namespace acid
 
 	void Texture::CreateMipmaps(const VkImage &image, const int32_t &width, const int32_t &height, const int32_t &depth, const uint32_t &mipLevels, const uint32_t &layerCount)
 	{
-		/*if (mipLevels == 1)
-		{
-			return;
-		}*/
-
 		CommandBuffer commandBuffer = CommandBuffer();
 
 		VkImageMemoryBarrier barrier = {};
@@ -527,5 +529,171 @@ namespace acid
 		imageViewCreateInfo.subresourceRange.layerCount = layerCount;
 
 		Display::CheckVk(vkCreateImageView(logicalDevice, &imageViewCreateInfo, nullptr, &imageView));
+	}
+
+	bool Texture::CopyImage(const VkImage &srcImage, VkImage &dstImage, VkDeviceMemory &dstImageMemory, const uint32_t &width, const uint32_t &height)
+	{
+		auto logicalDevice = Display::Get()->GetVkLogicalDevice();
+		auto physicalDevice = Display::Get()->GetVkPhysicalDevice();
+		auto surfaceFormat = Display::Get()->GetVkSurfaceFormat();
+		auto physicalDeviceMemoryProperties = Display::Get()->GetVkPhysicalDeviceMemoryProperties();
+
+		// Checks blit swapchain support.
+		bool supportsBlit = true;
+		VkFormatProperties formatProperties;
+
+		// Check if the device supports blitting from optimal images (the swapchain images are in optimal format).
+		vkGetPhysicalDeviceFormatProperties(physicalDevice, surfaceFormat.format, &formatProperties);
+
+		if (!(formatProperties.optimalTilingFeatures & VK_FORMAT_FEATURE_BLIT_SRC_BIT))
+		{
+			supportsBlit = false;
+		}
+
+		// Check if the device supports blitting to linear images.
+		vkGetPhysicalDeviceFormatProperties(physicalDevice, VK_FORMAT_R8G8B8A8_UNORM, &formatProperties);
+
+		if (!(formatProperties.linearTilingFeatures & VK_FORMAT_FEATURE_BLIT_DST_BIT))
+		{
+			supportsBlit = false;
+		}
+
+		// Create the linear tiled destination image to copy to and to read the memory from.
+		VkImageCreateInfo imageCreateInfo{};
+		imageCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+		imageCreateInfo.imageType = VK_IMAGE_TYPE_2D;
+		imageCreateInfo.format = VK_FORMAT_R8G8B8A8_UNORM;
+		imageCreateInfo.extent.width = width;
+		imageCreateInfo.extent.height = height;
+		imageCreateInfo.extent.depth = 1;
+		imageCreateInfo.arrayLayers = 1;
+		imageCreateInfo.mipLevels = 1;
+		imageCreateInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		imageCreateInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+		imageCreateInfo.tiling = VK_IMAGE_TILING_LINEAR;
+		imageCreateInfo.usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+
+		// Create the image.
+		Display::CheckVk(vkCreateImage(logicalDevice, &imageCreateInfo, nullptr, &dstImage));
+
+		// Create memory to back up the image.
+		VkMemoryRequirements memoryRequirements;
+		VkMemoryAllocateInfo memoryAllocateInfo{};
+		memoryAllocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+
+		vkGetImageMemoryRequirements(logicalDevice, dstImage, &memoryRequirements);
+		memoryAllocateInfo.allocationSize = memoryRequirements.size;
+
+		// Memory must be host visible to copy from.
+		memoryAllocateInfo.memoryTypeIndex = Renderer::FindMemoryTypeIndex(&physicalDeviceMemoryProperties, &memoryRequirements, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+		Display::CheckVk(vkAllocateMemory(logicalDevice, &memoryAllocateInfo, nullptr, &dstImageMemory));
+		Display::CheckVk(vkBindImageMemory(logicalDevice, dstImage, dstImageMemory, 0));
+
+		// Do the actual blit from the swapchain image to our host visible destination image.
+		CommandBuffer commandBuffer = CommandBuffer();
+
+		// Transition destination image to transfer destination layout.
+		InsertImageMemoryBarrier(
+			commandBuffer.GetVkCommandBuffer(),
+			dstImage,
+			VK_ACCESS_TRANSFER_WRITE_BIT,
+			VK_ACCESS_MEMORY_READ_BIT,
+			VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+			VK_IMAGE_LAYOUT_GENERAL,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VkImageSubresourceRange{VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1});
+
+		// Transition swapchain image from present to transfer source layout
+		InsertImageMemoryBarrier(
+			commandBuffer.GetVkCommandBuffer(),
+			srcImage,
+			VK_ACCESS_TRANSFER_READ_BIT,
+			VK_ACCESS_MEMORY_READ_BIT,
+			VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+			VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VkImageSubresourceRange{VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1});
+
+		// If source and destination support blit we'll blit as this also does automatic format conversion (e.g. from BGR to RGB).
+		if (supportsBlit)
+		{
+			// Define the region to blit (we will blit the whole swapchain image).
+			VkOffset3D blitSize;
+			blitSize.x = width;
+			blitSize.y = height;
+			blitSize.z = 1;
+			VkImageBlit imageBlitRegion{};
+			imageBlitRegion.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			imageBlitRegion.srcSubresource.layerCount = 1;
+			imageBlitRegion.srcOffsets[1] = blitSize;
+			imageBlitRegion.dstSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			imageBlitRegion.dstSubresource.layerCount = 1;
+			imageBlitRegion.dstOffsets[1] = blitSize;
+
+			// Issue the blit command
+			vkCmdBlitImage(commandBuffer.GetVkCommandBuffer(), srcImage, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, dstImage, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &imageBlitRegion, VK_FILTER_NEAREST);
+		}
+		else
+		{
+			// Otherwise use image copy (requires us to manually flip components).
+			VkImageCopy imageCopyRegion{};
+			imageCopyRegion.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			imageCopyRegion.srcSubresource.layerCount = 1;
+			imageCopyRegion.dstSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			imageCopyRegion.dstSubresource.layerCount = 1;
+			imageCopyRegion.extent.width = static_cast<uint32_t>(width);
+			imageCopyRegion.extent.height = static_cast<uint32_t>(height);
+			imageCopyRegion.extent.depth = 1;
+
+			// Issue the copy command.
+			vkCmdCopyImage(commandBuffer.GetVkCommandBuffer(), srcImage, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, dstImage, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &imageCopyRegion);
+		}
+
+		// Transition destination image to general layout, which is the required layout for mapping the image memory later on
+		InsertImageMemoryBarrier(
+			commandBuffer.GetVkCommandBuffer(),
+			dstImage,
+			VK_ACCESS_TRANSFER_WRITE_BIT,
+			VK_ACCESS_MEMORY_READ_BIT,
+			VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+			VK_IMAGE_LAYOUT_GENERAL,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VkImageSubresourceRange{VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1});
+
+		// Transition back the swap chain image after the blit is done
+		InsertImageMemoryBarrier(
+			commandBuffer.GetVkCommandBuffer(),
+			srcImage,
+			VK_ACCESS_TRANSFER_READ_BIT,
+			VK_ACCESS_MEMORY_READ_BIT,
+			VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+			VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VK_PIPELINE_STAGE_TRANSFER_BIT,
+			VkImageSubresourceRange{VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1});
+
+		commandBuffer.End();
+		commandBuffer.Submit();
+
+		return supportsBlit;
+	}
+
+	void Texture::InsertImageMemoryBarrier(const VkCommandBuffer &cmdbuffer, const VkImage &image, const VkAccessFlags &srcAccessMask, const VkAccessFlags &dstAccessMask, const VkImageLayout &oldImageLayout, const VkImageLayout &newImageLayout, const VkPipelineStageFlags &srcStageMask, const VkPipelineStageFlags &dstStageMask, const VkImageSubresourceRange &subresourceRange)
+	{
+		VkImageMemoryBarrier imageMemoryBarrier{};
+		imageMemoryBarrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+		imageMemoryBarrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+		imageMemoryBarrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+		imageMemoryBarrier.srcAccessMask = srcAccessMask;
+		imageMemoryBarrier.dstAccessMask = dstAccessMask;
+		imageMemoryBarrier.oldLayout = oldImageLayout;
+		imageMemoryBarrier.newLayout = newImageLayout;
+		imageMemoryBarrier.image = image;
+		imageMemoryBarrier.subresourceRange = subresourceRange;
+
+		vkCmdPipelineBarrier(cmdbuffer, srcStageMask, dstStageMask, 0, 0, nullptr, 0, nullptr, 1, &imageMemoryBarrier);
 	}
 }
