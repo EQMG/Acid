3e810a51eb51c4b9bfd1bb460db225465011654f
Matthew Albrecht <mattparks5855@gmail.com>
2019-03-14 15:13:15 -0500

    Resources now linked on build to the bin folder.

--- Scripts/update-modules.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd ../
-
-git submodule update --init --recursive
-git submodule foreach git fetch origin
-git submodule foreach git reset --hard origin/master
-git submodule foreach git pull origin master
