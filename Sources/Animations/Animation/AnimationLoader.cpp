EQMG/Acid

commit b4cb56d99c17ce3d052a1931698cf1a2369c9472

Implemented C++20 feature: enumerate.

CMakeLists.txt
@@ -174,8 +174,8 @@ find_package(Bullet 2.89 QUIET)
 if(NOT BULLET_FOUND)
 	set(_ACID_ALL_SYSTEM_LIBS false)
 	FetchContent_Declare(bullet3
-			URL https://github.com/bulletphysics/bullet3/archive/28039903b14c2aec28beff5b3609c9308b17e76a.tar.gz
-			URL_MD5 22af127b8be365539fba731af6c1e700
+			URL https://github.com/bulletphysics/bullet3/archive/2.89.tar.gz
+			URL_MD5 d239b4800ec30513879834be6fcdc376
 			)
 	FetchContent_GetProperties(bullet3)
 	if(NOT bullet3_POPULATED)

Sources/Animations/Animation/AnimationLoader.cpp
@@ -1,6 +1,7 @@
 #include "AnimationLoader.hpp"
 
 #include "Animations/MeshAnimated.hpp"
+#include "Helpers/Enumerate.hpp"
 
 namespace acid {
 AnimationLoader::AnimationLoader(NodeView &&libraryAnimations, NodeView &&libraryVisualScenes, const Matrix4 &correction) :
@@ -65,7 +66,7 @@ std::string AnimationLoader::GetJointName(const Node &jointData) {
 }
 
 void AnimationLoader::ProcessTransforms(const std::string &jointName, const std::vector<std::string> &rawData, bool root) {
-	for (uint32_t i = 0; i < m_keyframes.size(); i++) {
+	for (auto &[i, keyframe] : Enumerate(m_keyframes)) {
 		Matrix4 transform;
 
 		for (uint32_t row = 0; row < 4; row++) {
@@ -78,7 +79,7 @@ void AnimationLoader::ProcessTransforms(const std::string &jointName, const std:
 		if (root)
 			transform = m_correction * transform;
 
-		m_keyframes[i].AddJointTransform(jointName, transform);
+		keyframe.AddJointTransform(jointName, transform);
 	}
 }
 }

Sources/Animations/Skin/SkinLoader.cpp
@@ -1,5 +1,7 @@
 #include "SkinLoader.hpp"
 
+#include "Helpers/Enumerate.hpp"
+
 namespace acid {
 SkinLoader::SkinLoader(NodeView libraryControllers, uint32_t maxWeights) :
 	m_skinData(libraryControllers["controller"]["skin"]),
@@ -27,8 +29,8 @@ std::vector<float> SkinLoader::LoadWeights() const {
 	auto rawDatas = String::Split(weightsNode->Get<std::string>(), ' ');
 	std::vector<float> weights(rawDatas.size());
 
-	for (uint32_t i = 0; i < weights.size(); i++) {
-		weights[i] = String::From<float>(rawDatas[i]);
+	for (auto &[i, weight] : Enumerate(weights)) {
+		weight = String::From<float>(rawDatas[i]);
 	}
 
 	return weights;
@@ -38,8 +40,8 @@ std::vector<uint32_t> SkinLoader::GetEffectiveJointsCounts(const Node &weightsDa
 	auto rawData = String::Split(weightsDataNode["vcount"]->Get<std::string>(), ' ');
 	std::vector<uint32_t> counts(rawData.size());
 
-	for (uint32_t i = 0; i < rawData.size(); i++) {
-		counts[i] = String::From<uint32_t>(rawData[i]);
+	for (auto &[i, count] : Enumerate(counts)) {
+		count = String::From<uint32_t>(rawData[i]);
 	}
 
 	return counts;

Sources/Animations/Skin/VertexWeights.cpp
@@ -1,17 +1,19 @@
 #include "VertexWeights.hpp"
 
+#include "Helpers/Enumerate.hpp"
+
 namespace acid {
-void VertexWeights::AddJointEffect(uint32_t jointId, float weight) {
-	for (uint32_t i = 0; i < m_weights.size(); i++) {
-		if (weight > m_weights.at(i)) {
+void VertexWeights::AddJointEffect(uint32_t jointId, float jointWeight) {
+	for (auto &&[i, weight] : Enumerate(m_weights)) {
+		if (jointWeight > weight) {
 			m_jointIds[i] = jointId;
-			m_weights[i] = weight;
+			weight = jointWeight;
 			return;
 		}
 	}
 
 	m_jointIds.emplace_back(jointId);
-	m_weights.emplace_back(weight);
+	m_weights.emplace_back(jointWeight);
 }
 
 void VertexWeights::LimitJointNumber(uint32_t max) {
@@ -35,9 +37,9 @@ void VertexWeights::FillEmptyWeights(uint32_t max) {
 float VertexWeights::SaveTopWeights(std::vector<float> &topWeightsArray) {
 	float total = 0.0f;
 
-	for (uint32_t i = 0; i < topWeightsArray.size(); i++) {
-		topWeightsArray[i] = m_weights.at(i);
-		total += topWeightsArray.at(i);
+	for (auto &[i, topWeights] : Enumerate(topWeightsArray)) {
+		topWeights = m_weights.at(i);
+		total += topWeights;
 	}
 
 	return total;

Sources/Fonts/Outline.cpp
@@ -3,7 +3,9 @@
 #include <ft2build.h>
 #include FT_BBOX_H
 #include FT_OUTLINE_H
+
 #include "Maths/Maths.hpp"
+#include "Helpers/Enumerate.hpp"
 
 namespace acid {
 static const uint32_t FD_OUTLINE_MAX_POINTS = 255 * 2;
@@ -49,16 +51,11 @@ void Outline::Decompose(FT_Outline *outline) {
 void Outline::FixCorners() {
 	auto fixDist = 0.001f;
 
-	for (uint32_t contourIndex = 0; contourIndex < m_contours.size(); contourIndex++) {
-		auto contourBegin = m_contours[contourIndex].m_begin;
-		auto contourEnd = m_contours[contourIndex].m_end;
-
-		for (auto i = contourBegin; i < contourEnd; i += 2) {
+	for (auto &[contourIndex, contour] : Enumerate(m_contours)) {
+		for (auto i = contour.m_begin; i < contour.m_end; i += 2) {
 			auto prev = i - 1;
-
-			if (contourBegin == i) {
-				prev = contourEnd - 1;
-			}
+			if (contour.m_begin == i)
+				prev = contour.m_end - 1;
 
 			auto &r = m_points[prev];
 			auto &p0 = m_points[i];
@@ -96,10 +93,7 @@ void Outline::Subdivide() {
 	Outline u = {};
 	u.m_bbox = m_bbox;
 
-	for (uint32_t contourIndex = 0; contourIndex < m_contours.size(); contourIndex++) {
-		auto contourBegin = m_contours[contourIndex].m_begin;
-		auto contour_end = m_contours[contourIndex].m_end;
-
+	for (auto &[contourIndex, contour] : Enumerate(m_contours)) {
 		u.AddOddPoint();
 
 		ContourRange urange = {
@@ -108,7 +102,7 @@ void Outline::Subdivide() {
 		};
 		u.m_contours.emplace_back(urange);
 
-		for (uint32_t i = contourBegin; i < contour_end; i += 2) {
+		for (uint32_t i = contour.m_begin; i < contour.m_end; i += 2) {
 			auto &p0 = m_points[i];
 			//auto &p1 = points[i + 1];
 			//auto &p2 = points[i + 2];
@@ -123,7 +117,7 @@ void Outline::Subdivide() {
 		}
 
 		u.m_contours[contourIndex].m_end = static_cast<uint32_t>(u.m_points.size());
-		u.m_points.emplace_back(m_points[contour_end]);
+		u.m_points.emplace_back(m_points[contour.m_end]);
 	}
 
 	*this = u;
@@ -134,10 +128,7 @@ void Outline::FixThinLines() {
 	Outline u = {};
 	u.m_bbox = m_bbox;
 
-	for (uint32_t contourIndex = 0; contourIndex < m_contours.size(); contourIndex++) {
-		auto contourBegin = m_contours[contourIndex].m_begin;
-		auto contourEnd = m_contours[contourIndex].m_end;
-
+	for (auto &[contourIndex, contour] : Enumerate(m_contours)) {
 		u.AddOddPoint();
 
 		ContourRange urange = {
@@ -146,7 +137,7 @@ void Outline::FixThinLines() {
 		};
 		u.m_contours.emplace_back(urange);
 
-		for (auto i = contourBegin; i < contourEnd; i += 2) {
+		for (auto i = contour.m_begin; i < contour.m_end; i += 2) {
 			auto &p0 = m_points[i];
 			auto &p1 = m_points[i + 1];
 			auto &p2 = m_points[i + 2];
@@ -193,12 +184,12 @@ void Outline::FixThinLines() {
 
 			bool subdivide = false;
 
-			for (auto j = contourBegin; j < contourEnd; j += 2) {
-				if (i == contourBegin && j == contourEnd - 2) {
+			for (auto j = contour.m_begin; j < contour.m_end; j += 2) {
+				if (i == contour.m_begin && j == contour.m_end - 2) {
 					continue;
 				}
 
-				if (i == contourEnd - 2 && j == contourBegin) {
+				if (i == contour.m_end - 2 && j == contour.m_begin) {
 					continue;
 				}
 
@@ -230,7 +221,7 @@ void Outline::FixThinLines() {
 		}
 
 		u.m_contours[contourIndex].m_end = static_cast<uint32_t>(u.m_points.size());
-		u.m_points.emplace_back(m_points[contourEnd]);
+		u.m_points.emplace_back(m_points[contour.m_end]);
 	}
 
 	*this = u;
@@ -377,12 +368,9 @@ Rect Outline::GetCbox() const {
 Rect Outline::GetU16Points(Vector2us *pout) const {
 	auto cbox = GetCbox();
 
-	for (uint32_t i = 0; i < m_points.size(); i++) {
-		auto x = m_points[i].m_x;
-		auto y = m_points[i].m_y;
-
-		pout[i].m_x = GenU16Value(x, cbox.m_min.m_x, cbox.m_max.m_x);
-		pout[i].m_y = GenU16Value(y, cbox.m_min.m_y, cbox.m_max.m_y);
+	for (auto &[i, point] : Enumerate(m_points)) {
+		pout[i].m_x = GenU16Value(point.m_x, cbox.m_min.m_x, cbox.m_max.m_x);
+		pout[i].m_y = GenU16Value(point.m_y, cbox.m_min.m_y, cbox.m_max.m_y);
 	}
 
 	return cbox;
@@ -648,19 +636,16 @@ bool Outline::TryToFitInCellCount() {
 	u.m_cellCount.m_x = m_cellCount.m_x;
 	u.m_cellCount.m_y = m_cellCount.m_y;
 
-	for (uint32_t contourIndex = 0; contourIndex < m_contours.size(); contourIndex++) {
-		auto contourBegin = m_contours[contourIndex].m_begin;
-		auto contourEnd = m_contours[contourIndex].m_end;
-
+	for (auto &[contourIndex, contour] : Enumerate(m_contours)) {
 		u.AddOddPoint();
 
 		ContourRange urange = {
 			static_cast<uint32_t>(u.m_points.size()),
-			static_cast<uint32_t>(u.m_points.size()) + contourEnd - contourBegin
+			static_cast<uint32_t>(u.m_points.size()) + contour.m_end - contour.m_begin
 		};
 		u.m_contours.emplace_back(urange);
 
-		for (uint32_t i = contourBegin; i < contourEnd; i += 2) {
+		for (uint32_t i = contour.m_begin; i < contour.m_end; i += 2) {
 			auto &p0 = m_points[i];
 			auto &p1 = m_points[i + 1];
 			//auto &p2 = points[i + 2];
@@ -675,9 +660,9 @@ bool Outline::TryToFitInCellCount() {
 		uint32_t maxStartLen = 0;
 		result &= WipCell::ForEachWipcellFinishContour(this, &u, contourIndex, cells.data(), maxStartLen);
 
-		auto continuationEnd = contourBegin + maxStartLen * 2;
+		auto continuationEnd = contour.m_begin + maxStartLen * 2;
 
-		for (uint32_t i = contourBegin; i < continuationEnd; i += 2) {
+		for (uint32_t i = contour.m_begin; i < continuationEnd; i += 2) {
 			u.m_points.emplace_back(m_points[i]);
 			u.m_points.emplace_back(m_points[i + 1]);
 		}

Sources/Animations/Animator.cpp
@@ -1,6 +1,7 @@
 #include "Animator.hpp"
 
 #include "Engine/Engine.hpp"
+#include "Helpers/Enumerate.hpp"
 
 namespace acid {
 void Animator::Update(const Joint &rootJoint, std::vector<Matrix4> &jointMatrices) {
@@ -28,21 +29,19 @@ std::map<std::string, Matrix4> Animator::CalculateCurrentAnimationPose() const {
 }
 
 std::pair<Keyframe, Keyframe> Animator::GetPreviousAndNextFrames() const {
-	auto allFrames = m_currentAnimation->GetKeyframes();
-	auto previousFrame = allFrames[0];
-	auto nextFrame = allFrames[0];
+	const auto &allFrames = m_currentAnimation->GetKeyframes();
+	const Keyframe *previousFrame = nullptr;
+	const Keyframe *nextFrame = nullptr;
 
-	for (uint32_t i = 1; i < allFrames.size(); i++) {
-		nextFrame = allFrames[i];
-
-		if (nextFrame.GetTimeStamp() > m_animationTime) {
+	for (const auto &[i, frame] : Enumerate(allFrames)) {
+		nextFrame = &frame;
+		if (frame.GetTimeStamp() > m_animationTime)
 			break;
-		}
 
-		previousFrame = allFrames[i];
+		previousFrame = &frame;
 	}
 
-	return {previousFrame, nextFrame};
+	return {*previousFrame, *nextFrame};
 }
 
 float Animator::CalculateProgression(const Keyframe &previousFrame, const Keyframe &nextFrame) const {

Sources/Animations/Skeleton/SkeletonLoader.cpp
@@ -1,6 +1,7 @@
 #include "SkeletonLoader.hpp"
 
 #include "Animations/MeshAnimated.hpp"
+#include "Helpers/Enumerate.hpp"
 
 namespace acid {
 SkeletonLoader::SkeletonLoader(NodeView &&libraryControllers, std::vector<std::string> boneOrder, const Matrix4 &correction) :
@@ -54,9 +55,9 @@ Joint SkeletonLoader::ExtractMainJointData(const Node &jointNode, bool isRoot) {
 	return {*index, nameId, transform};
 }
 
-std::optional<uint32_t> SkeletonLoader::GetBoneIndex(const std::string &name) {
-	for (uint32_t i = 0; i < m_boneOrder.size(); i++) {
-		if (m_boneOrder[i] == name) {
+std::optional<uint32_t> SkeletonLoader::GetBoneIndex(const std::string &name) const {
+	for (const auto &[i, bone] : Enumerate(m_boneOrder)) {
+		if (bone == name) {
 			return i;
 		}
 	}

Sources/Animations/Skeleton/SkeletonLoader.hpp
@@ -14,7 +14,7 @@ public:
 private:
 	Joint LoadJointData(const Node &jointNode, bool isRoot);
 	Joint ExtractMainJointData(const Node &jointNode, bool isRoot);
-	std::optional<uint32_t> GetBoneIndex(const std::string &name);
+	std::optional<uint32_t> GetBoneIndex(const std::string &name) const;
 
 	NodeView m_armatureData;
 	std::vector<std::string> m_boneOrder;

commit cad2c619e31e0bc66c42dc2ebaa4036d200c0735

Fixed Clang issues introduced in b4cb56d99c17ce3d052a1931698cf1a2369c9472

CMakeLists.txt
@@ -170,6 +170,9 @@ if(NOT FREETYPE_FOUND)
 	set(FREETYPE_LIBRARIES "freetype")
 endif()
 
+if(WIN32 AND (CMAKE_CXX_COMPILER_ID STREQUAL "Clang"))
+	set(CMAKE_DISABLE_FIND_PACKAGE_Bullet TRUE CACHE INTERNAL "")
+endif()
 find_package(Bullet 2.89 QUIET)
 if(NOT BULLET_FOUND)
 	set(_ACID_ALL_SYSTEM_LIBS false)

Sources/Animations/Animation/AnimationLoader.cpp
@@ -66,7 +66,7 @@ std::string AnimationLoader::GetJointName(const Node &jointData) {
 }
 
 void AnimationLoader::ProcessTransforms(const std::string &jointName, const std::vector<std::string> &rawData, bool root) {
-	for (auto &[i, keyframe] : Enumerate(m_keyframes)) {
+	for (auto &&[i, keyframe] : Enumerate(m_keyframes)) {
 		Matrix4 transform;
 
 		for (uint32_t row = 0; row < 4; row++) {

Sources/Animations/Skin/SkinLoader.cpp
@@ -29,7 +29,7 @@ std::vector<float> SkinLoader::LoadWeights() const {
 	auto rawDatas = String::Split(weightsNode->Get<std::string>(), ' ');
 	std::vector<float> weights(rawDatas.size());
 
-	for (auto &[i, weight] : Enumerate(weights)) {
+	for (auto &&[i, weight] : Enumerate(weights)) {
 		weight = String::From<float>(rawDatas[i]);
 	}
 
@@ -40,7 +40,7 @@ std::vector<uint32_t> SkinLoader::GetEffectiveJointsCounts(const Node &weightsDa
 	auto rawData = String::Split(weightsDataNode["vcount"]->Get<std::string>(), ' ');
 	std::vector<uint32_t> counts(rawData.size());
 
-	for (auto &[i, count] : Enumerate(counts)) {
+	for (auto &&[i, count] : Enumerate(counts)) {
 		count = String::From<uint32_t>(rawData[i]);
 	}
 

Sources/Animations/Skin/VertexWeights.cpp
@@ -37,7 +37,7 @@ void VertexWeights::FillEmptyWeights(uint32_t max) {
 float VertexWeights::SaveTopWeights(std::vector<float> &topWeightsArray) {
 	float total = 0.0f;
 
-	for (auto &[i, topWeights] : Enumerate(topWeightsArray)) {
+	for (auto &&[i, topWeights] : Enumerate(topWeightsArray)) {
 		topWeights = m_weights.at(i);
 		total += topWeights;
 	}

Sources/Fonts/Outline.cpp
@@ -51,7 +51,7 @@ void Outline::Decompose(FT_Outline *outline) {
 void Outline::FixCorners() {
 	auto fixDist = 0.001f;
 
-	for (auto &[contourIndex, contour] : Enumerate(m_contours)) {
+	for (auto &&[contourIndex, contour] : Enumerate(m_contours)) {
 		for (auto i = contour.m_begin; i < contour.m_end; i += 2) {
 			auto prev = i - 1;
 			if (contour.m_begin == i)
@@ -93,7 +93,7 @@ void Outline::Subdivide() {
 	Outline u = {};
 	u.m_bbox = m_bbox;
 
-	for (auto &[contourIndex, contour] : Enumerate(m_contours)) {
+	for (auto &&[contourIndex, contour] : Enumerate(m_contours)) {
 		u.AddOddPoint();
 
 		ContourRange urange = {
@@ -128,7 +128,7 @@ void Outline::FixThinLines() {
 	Outline u = {};
 	u.m_bbox = m_bbox;
 
-	for (auto &[contourIndex, contour] : Enumerate(m_contours)) {
+	for (auto &&[contourIndex, contour] : Enumerate(m_contours)) {
 		u.AddOddPoint();
 
 		ContourRange urange = {
@@ -368,7 +368,7 @@ Rect Outline::GetCbox() const {
 Rect Outline::GetU16Points(Vector2us *pout) const {
 	auto cbox = GetCbox();
 
-	for (auto &[i, point] : Enumerate(m_points)) {
+	for (auto &&[i, point] : Enumerate(m_points)) {
 		pout[i].m_x = GenU16Value(point.m_x, cbox.m_min.m_x, cbox.m_max.m_x);
 		pout[i].m_y = GenU16Value(point.m_y, cbox.m_min.m_y, cbox.m_max.m_y);
 	}
@@ -636,7 +636,7 @@ bool Outline::TryToFitInCellCount() {
 	u.m_cellCount.m_x = m_cellCount.m_x;
 	u.m_cellCount.m_y = m_cellCount.m_y;
 
-	for (auto &[contourIndex, contour] : Enumerate(m_contours)) {
+	for (auto &&[contourIndex, contour] : Enumerate(m_contours)) {
 		u.AddOddPoint();
 
 		ContourRange urange = {